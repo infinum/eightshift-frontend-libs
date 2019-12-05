f6d55ce973eaddbc4c7ead4a41f7d5bdbf89cf86
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2019-12-05 16:17:28 +0100

    small fixes

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,6 +7,7 @@ import { dynamicImport } from 'EighshiftBlocksDynamicImport';
 import { Wrapper } from './../blocks/init/wrapper/wrapper';
 import blocksSettings from './../blocks/init/manifest.json';
 
+// Loading WP build files.
 window.wp.element = require('./../node_modules/@wordpress/element/build-module');
 window.wp.compose = require('./../node_modules/@wordpress/compose/build-module');
 window.wp.hooks = require('./../node_modules/@wordpress/hooks/build-module');
@@ -23,6 +24,7 @@ window.wp.hooks = require('./../node_modules/@wordpress/hooks/build-module');
 // window.wp.priorityQueue = require('./../node_modules/@wordpress/priority-queue/build-module');
 // window.wp.plugins = require('./../node_modules/@wordpress/plugins/build-module');
 
+// Register Editor Blocks.
 registerBlocks(
   require.context('./../blocks/init/custom', true, /manifest\.json$/),
   require.context('./../blocks/init/custom', true, /\.js$/),
@@ -33,6 +35,7 @@ registerBlocks(
 // Find all blocks hooks require hooks index.js inside it.
 dynamicImport(require.context('./../blocks/init/custom', true, /hooks\/index.js$/));
 
+// Run all storybook stories.
 configure([
   require.context("./../blocks/init/components", true, /story.js$/),
   require.context("./../blocks/init/custom", true, /story.js$/),
