30c3f48aa4913325b3e6bf939d2b6eba04023148
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2019-12-11 11:50:28 +0100

    adding docs

--- storybook/vendors~main.f2ef17147bf15483a621.bundle.js
+++ storybook/vendors~main.f2ef17147bf15483a621.bundle.js
@@ -174,4 +174,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.361dccc0b545c5f2b9c4.bundle.js.map
+//# sourceMappingURL=vendors~main.f2ef17147bf15483a621.bundle.js.map
