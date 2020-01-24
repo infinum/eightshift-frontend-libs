722e3304f1b25e9b9a737b569f199e03b736c898
Ivan Grginov <ivan.grginov@infinum.hr>
2020-01-24 08:01:30 +0100

    Trying to get tests running with a specific branch

--- tests/integration/create-wp-project/create-wp-project-plugin.test.js
+++ tests/integration/create-wp-project/create-wp-project-plugin.test.js
@@ -7,12 +7,9 @@ const { exec } = require('promisify-child-process');
 const { ensureDir } = require('fs-extra');
 const del = require('del');
 
+const { log } = console;
 const timeout = 180000;
 const testingFolder = './temp-plugin';
-
-// When this runs on Travis, make sure to override boilerplate's frontend-libs version
-// with the Pull Request branch.
-const overrideBranch = process.env.TRAVIS_PULL_REQUEST_BRANCH ? ` eightshiftLibsBranch="${process.env.TRAVIS_PULL_REQUEST_BRANCH}"` : '';
 
 beforeEach(async () => {
   await del(testingFolder);
@@ -23,9 +20,15 @@ afterEach(async () => {
   await del(testingFolder);
 }, timeout);
 
-it('tests create-wp-project script (npx version) - creates a plugin', async() => { // eslint-disable-line jest/no-focused-tests
+it('tests create-wp-project script (npx version) - creates a plugin', async() => {
+
+  // When this runs on Travis, make sure to override boilerplate's frontend-libs version
+  // with the Pull Request branch.
+  log('--- Debug environment vars: ', process.env);
+  const overrideBranch = process.env.TRAVIS_PULL_REQUEST_BRANCH ? ` eightshiftLibsBranch="${process.env.TRAVIS_PULL_REQUEST_BRANCH}"` : '';
+
   const command = `npx create-wp-project@latest plugin --projectName="Test plugin" --url="eightshift.local" --description="This is a description for a plugin" --noSummary ${overrideBranch}`;
-  console.log('Running command: ', command);
+  log('Running command: ', command);
   const { err } = await exec(`cd ${testingFolder} && ${command}`);
 
   expect(err).toBeFalsy();
--- tests/integration/create-wp-project/create-wp-project-theme.test.js
+++ tests/integration/create-wp-project/create-wp-project-theme.test.js
@@ -7,12 +7,9 @@ const { exec } = require('promisify-child-process');
 const { ensureDir } = require('fs-extra');
 const del = require('del');
 
+const { log } = console;
 const timeout = 180000;
 const testingFolder = './temp-theme';
-
-// When this runs on Travis, make sure to override boilerplate's frontend-libs version
-// with the Pull Request branch.
-const overrideBranch = process.env.TRAVIS_PULL_REQUEST_BRANCH ? ` eightshiftLibsBranch="${process.env.TRAVIS_PULL_REQUEST_BRANCH}"` : '';
 
 beforeEach(async () => {
   await del(testingFolder);
@@ -23,9 +20,15 @@ afterEach(async () => {
   await del(testingFolder);
 }, timeout);
 
-it('tests create-wp-project script (npx version) - creates a theme', async() => { // eslint-disable-line jest/no-focused-tests
+it('tests create-wp-project script (npx version) - creates a theme', async() => {
+
+  // When this runs on Travis, make sure to override boilerplate's frontend-libs version
+  // with the Pull Request branch.
+  log('--- Debug environment vars: ', process.env);
+  const overrideBranch = process.env.TRAVIS_PULL_REQUEST_BRANCH ? ` eightshiftLibsBranch="${process.env.TRAVIS_PULL_REQUEST_BRANCH}"` : '';
+
   const command = `npx create-wp-project@latest --projectName="Test theme" --url="eightshift.local" --description="This is a description for a theme" --noSummary ${overrideBranch}`;
-  console.log('Running command: ', command);
+  log('Running command: ', command);
   const { err } = await exec(`cd ${testingFolder} && ${command}`);
 
   expect(err).toBeFalsy();
