ec35cb8022d5493c3e7970f616159f54a29505e2
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-04-28 02:09:29 +0200

    updating scripts

--- blocks/init/storybook/config.js
+++ blocks/init/storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from '@storybook/react';
-import { storybookDefaultMocks, storybookWindowObjects, storybookWpStyles } from '@eightshift/frontend-libs/.storybook/parts';
+import { storybookDefaultMocks, storybookWindowObjects, storybookWpStyles } from '@eightshift/frontend-libs/scripts/storybook';
 
 // Storybook order is really important because it won't work in any configuration. Be careful when changing stuff here.
 
