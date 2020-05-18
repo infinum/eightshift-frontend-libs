beec2d17afaf4df0fd509246a53d22a08735ccf5
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-05-18 11:29:38 +0200

    linter fix

--- blocks/init/src/blocks/custom/image/transforms.js
+++ blocks/init/src/blocks/custom/image/transforms.js
@@ -1,6 +1,6 @@
 import { createBlock } from '@wordpress/blocks';
 import manifest from './../../manifest.json';
-import manifestParagraph from './manifest.json';
+import manifestImage from './manifest.json';
 import manifestCarouselImage from './../carousel-image/manifest.json';
 
 export const Transforms = {
@@ -9,7 +9,7 @@ export const Transforms = {
       type: 'block',
       blocks: [`${manifest.namespace}/${manifestCarouselImage.blockName}`],
       transform: ({ media }) => (
-        createBlock(`${manifest.namespace}/${manifestParagraph.blockName}`, {
+        createBlock(`${manifest.namespace}/${manifestImage.blockName}`, {
           media,
         })
       ),
