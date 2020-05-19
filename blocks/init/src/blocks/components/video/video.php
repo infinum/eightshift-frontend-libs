344d74f9c1717d8ee73ec598f243e0d68287fa59
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-05-19 15:29:25 +0200

    adding additional check for components

--- blocks/init/src/blocks/components/video/video.php
+++ blocks/init/src/blocks/components/video/video.php
@@ -24,7 +24,7 @@ $block_class = $attributes['blockClass'] ?? '';
 ?>
 
 <video
-  src="<?php echo esc_url( $media['url'] ); ?>"
+  src="<?php echo esc_url( $url ); ?>"
   class="<?php echo esc_attr( "video {$block_class}__video" ); ?>"
   autoplay
   loop
