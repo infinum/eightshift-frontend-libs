f91a061f028c499ef9d148b2eccce5498b678fa9
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-06-08 06:43:32 +0200

    updating storybook style

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,36 +2,14 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-
-  // colorPrimary: 'hotpink',
-  // colorSecondary: 'deepskyblue',
-
-  // // UI
-  // appBg: 'white',
-  // appContentBg: 'silver',
-  // appBorderColor: 'grey',
-  // appBorderRadius: 4,
-
-  // // Typography
-  // fontBase: '"Open Sans", sans-serif',
-  // fontCode: 'monospace',
-
-  // // Text colors
-  // textColor: 'black',
-  // textInverseColor: 'rgba(255,255,255,0.9)',
-
-  // // Toolbar default and active colors
-  // barTextColor: 'silver',
-  // barSelectedColor: 'black',
-  // barBg: 'hotpink',
-
-  // // Form colors
-  // inputBg: 'white',
-  // inputBorder: 'silver',
-  // inputTextColor: 'black',
-  // inputBorderRadius: 4,
-
-  // brandTitle: 'My custom storybook',
-  // brandUrl: 'https://example.com',
-  // brandImage: 'https://placehold.it/350x150',
+  colorPrimary: '#0C3636',
+  colorSecondary: '#E84E4E',
+  appBg: '#F4F3F0',
+  appBorderRadius: 0,
+  fontBase: '"IBM Plex Sans", sans-serif',
+  textColor: '#0C3636',
+  textInverseColor: '#FFFFFF',
+  brandTitle: ' ',
+  brandUrl: '#',
+  brandImage: '',
 });
