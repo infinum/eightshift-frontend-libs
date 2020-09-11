441e193609f558b15134a92e6436a43f75e01e58
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-09-11 14:08:04 +0200

    update

--- blocks/init/src/blocks/components/tracking/before-body-end.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Tracking codes Before body end
- *
- * @package EightshiftBoilerplate
- */
-
-// Add Before body end hooks here.
--- blocks/init/src/blocks/components/tracking/head.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Tracking codes in Head
- *
- * @package EightshiftBoilerplate
- */
-
-get_template_part( 'src/blocks/components/tracking/parts/facebook' );
-get_template_part( 'src/blocks/components/tracking/parts/google-tag-manager' );
