57b6d459fbd71934541b36587112f7334fbf73b4
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-09-27 06:03:19 +0200

    update

--- blocks/init/src/Blocks/Custom/example/components/example-toolbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
-import { Fragment } from '@wordpress/element';
-
-export const ExampleToolbar = () => {
-  return (
-    <Fragment>
-    </Fragment>
-  );
-};
