83c0c3e6b8330b94fd7969b602de7eeeb3b363f1
Gabriel Glogoški <gabriel.glogoski@infinum.hr>
2020-12-14 17:54:59 +0100

    Add a helper to return height of the elements children

---
+++ scripts/helpers/element-children-height.js
@@ -0,0 +1,15 @@
+/**
+ * Returns height of the element measured by height of its children.
+ *
+ * @param object element DOM element
+ *
+ * @return Combined height of the children elements
+ */
+export function elementChildrenHeight(element) {
+	let height = 0;
+	[...element.children].forEach((child) => {
+		height += child.offsetHeight;
+	});
+
+	return height;
+}
