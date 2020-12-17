212d94d08280df6bb2b791958f7dcf9d7cc067bf
Gabriel Glogoški <gabriel.glogoski@infinum.hr>
2020-12-17 17:19:37 +0100

    Add share component

---
+++ blocks/init/src/Blocks/components/share/docs/readme.md
@@ -0,0 +1,33 @@
+# Share Component
+
+Component that allows user to share the current article on social networks. Currently Facebook, Twitter, LinkedIn and Piterest are supported.
+
+## Dependencies
+
+None
+
+## Attributes
+
+* _@param_ **blockClass** | `string` | default: `"share"` | Component's class. If modifying make sure the provide the default class as well (see example call).
+
+## Example call
+
+```php
+use Eightshift_Libs\Helpers\Components;
+
+Components::render( 'share' ) );
+
+// or
+
+Components::render( 'share', [
+	'blockClass' => 'share',
+] );
+```
+
+## Implementation
+
+Open a terminal and type in this command inside your projects root:
+
+```shell
+wp boilerplate use_component --name=share
+```
---
+++ blocks/init/src/Blocks/components/share/share.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Template for the Share Component view.
+ *
+ * @package EightshiftBoilerplate
+ */
+
+use EightshiftBoilerplateVendor\EightshiftLibs\Helpers\Components;
+
+$manifest = Components::getManifest(__DIR__);
+
+$postUrl = \get_the_permalink();
+$postTitle = \get_the_title();
+$postImage = \get_the_post_thumbnail_url(\get_the_ID(), 'large');
+
+$socialNetworks = [
+	'twitter' => "https://twitter.com/intent/tweet?url={$postUrl}&text={$postTitle}",
+	'linkedin' => "http://www.linkedin.com/shareArticle?mini=true&url={$postUrl}&title={$postTitle}",
+	'facebook' => "https://www.facebook.com/sharer/sharer.php?u={$postUrl}",
+	'pinterest' => "https://pinterest.com/pin/create/button/?url={$postUrl}&media={$postImage}&description={$postTitle}",
+];
+
+$componentClass = $attributes['componentClass'] ?? $manifest['componentClass'];
+$selectorClass = $attributes['selectorClass'] ?? $componentClass;
+$blockClass = $attributes['blockClass'] ?? '';
+
+$shareClass = Components::classnames([
+	$componentClass,
+	Components::selector($blockClass, $blockClass, "{$selectorClass}"),
+]);
+
+$shareItemClass = Components::classnames([
+	Components::selector($componentClass, $componentClass, 'item',),
+	Components::selector($blockClass, $blockClass, 'item',),
+	Components::selector($componentClass, "js-{$componentClass}-link"),
+]);
+
+?>
+<div class="<?php echo \esc_attr($shareClass); ?>">
+	<?php
+	foreach ($socialNetworks as $name => $url) {
+		?>
+		<a
+			href="<?php echo esc_url($url); ?>"
+			data-share-url="<?php echo esc_attr($postUrl);?>"
+			data-share-title="<?php echo esc_attr($postTitle);?>"
+			class="<?php echo esc_attr($shareItemClass); ?>"
+		>
+			<?php echo esc_html($name); ?>"
+		</a>
+		<?php
+	}
+	?>
+</div>
