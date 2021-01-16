3a1f0eb9d215f45dbc93deb68885f25b47a6eb3c
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-01-16 11:24:39 +0100

    updating fix for streching

--- scripts/helpers/camelize.js
+++ scripts/helpers/camelize.js
@@ -3,6 +3,9 @@
  *
  * @param string string Add string to convert.
  */
-export const camelize = (string) => {
-	return string.replace(/-./g, (x) => x[1].toUpperCase());
-};
+export const  camelize = (string) => {
+	string = string.toLowerCase().replace(/(?:(^.)|([-_\s]+.))/g, function(match) {
+			return match.charAt(match.length-1).toUpperCase();
+	});
+	return string.charAt(0).toLowerCase() + string.substring(1);
+}
