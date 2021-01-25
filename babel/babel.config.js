78792bdc3ecff843bfbed20be74106352d5c2b97
Denis Žoljom <denis.zoljom@gmail.com>
2021-01-25 12:32:18 +0100

    Add babel config
    In hope it will help

--- babel/babel.config.js
+++ babel/babel.config.js
@@ -17,4 +17,16 @@ module.exports = {
 			}
 		]
 	],
+	env: {
+		test: {
+			presets: [
+				[
+					'@babel/preset-env', {modules: 'commonjs'}
+				]
+			],
+			plugins: [
+				'@babel/plugin-transform-modules-commonjs'
+			]
+		}
+	}
 };
