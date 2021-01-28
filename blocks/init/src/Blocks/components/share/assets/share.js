e7616811bbcd63282f5b9c2cb59e79e1e0fd7a8d
Denis Žoljom <denis.zoljom@gmail.com>
2021-01-28 15:38:06 +0100

    Fix the sharer code to be async
    navigator API is async by design so Promise needs to be resolved.

--- blocks/init/src/Blocks/components/share/assets/share.js
+++ blocks/init/src/Blocks/components/share/assets/share.js
@@ -5,26 +5,26 @@ export class Share {
 
 	init() {
 		[...this.shareLinks].forEach((shareLink) => {
-			shareLink.addEventListener('click', (event) => {
+			shareLink.addEventListener('click', async (event) => {
 				event.preventDefault();
 
-				this.openShareDialog(event.currentTarget);
+				await this.openShareDialog(event.currentTarget);
 			});
 		});
 	}
 
-	openShareDialog(element) {
+	async openShareDialog(element) {
 		const shareUrl = element.getAttribute('href');
 		const pageUrl = element.getAttribute('data-share-url');
 		const pageTitle = element.getAttribute('data-share-title');
 
 		if (navigator.share) {
-			navigator.share({
+			await navigator.share({
 				title: pageTitle,
 				url: pageUrl,
 			});
 		} else {
-			window.open(
+			await window.open(
 				shareUrl,
 				'share-post',
 				'height=600,width=800,left=0,top=0,location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1'
