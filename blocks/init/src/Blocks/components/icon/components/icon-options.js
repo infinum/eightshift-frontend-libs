7e8fad25cd6566cf4995b025fa1d1e72d43ae460
Kristijan Maric <kristijan.maric@infinum.com>
2021-01-28 12:06:34 +0100

    add icon component

---
+++ blocks/init/src/Blocks/components/icon/components/icon-options.js
@@ -0,0 +1,74 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+import { SelectControl, ToggleControl, Icon } from '@wordpress/components';
+import { ColorPaletteCustom } from '@eightshift/frontend-libs/scripts/components';
+import { checkAttr } from '@eightshift/frontend-libs/scripts/helpers';
+import { getPaletteColors, icons } from '@eightshift/frontend-libs/scripts/editor';
+import manifest from './../manifest.json';
+
+const { options, title } = manifest;
+
+export const iconColors = () => {
+	const colors = getPaletteColors();
+
+	return [
+		colors.primary,
+		colors.black,
+		colors.light,
+		colors.white,
+	];
+};
+
+export const IconOptions = (attributes) => {
+	const {
+		setAttributes,
+		componentName = manifest.componentName,
+		label = title,
+		iconUse = checkAttr('iconUse', attributes, manifest, componentName),
+		iconName = checkAttr('iconName', attributes, manifest, componentName),
+		iconColor = checkAttr('iconColor', attributes, manifest, componentName),
+		showIconOptions = true,
+	} = attributes;
+
+	if (!showIconOptions) {
+		return null;
+	}
+
+	return (
+		<Fragment>
+			{label && <h3 className={'options-label'}>{label}</h3>}
+
+			<ToggleControl
+				label={__('Use Icon', 'eightshift-frontend-libs')}
+				checked={iconUse}
+				onChange={(value) => setAttributes({ [`${componentName}Use`]: value })}
+			/>
+
+			{iconUse && (
+				<Fragment>
+					<SelectControl
+						label={__('Icon', 'eightshift-frontend-libs')}
+						value={iconName}
+						options={options.icons}
+						onChange={(value) => setAttributes({ [`${componentName}Name`]: value })}
+					/>
+
+					<ColorPaletteCustom
+						label={
+							<Fragment>
+								<Icon icon={icons.color} />
+								{__('Icon color', 'eightshift-frontend-libs')}
+
+							</Fragment>
+						}
+						value={iconColor}
+						colors={iconColors()}
+						onChange={(value) => setAttributes({ [`${componentName}Color`]: value })}
+					/>
+				</Fragment>
+
+			)}
+		</Fragment>
+	);
+};
---
+++ blocks/init/src/Blocks/components/icon/docs/story.js
@@ -0,0 +1,37 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { Fragment } from '@wordpress/element';
+import readme from './readme.md';
+import manifest from './../manifest.json';
+import { IconEditor } from '../components/icon-editor';
+import { IconOptions } from '../components/icon-options';
+
+export default {
+	title: `Components/${manifest.title}`,
+	parameters: {
+		notes: readme,
+	},
+};
+
+const props = manifest.example.attributes;
+
+export const editor = () => (
+	<IconEditor {...props} />
+);
+
+export const options = () => (
+	<IconOptions {...props} />
+);
+
+export const color = () => (
+	<Fragment>
+		{manifest.options.colors.map((values, index) => (
+			<Fragment key={index}>
+				<IconEditor
+					{...props}
+					iconColor={values}
+				/>
+				<br />
+			</Fragment>
+		))}
+	</Fragment>
+);
