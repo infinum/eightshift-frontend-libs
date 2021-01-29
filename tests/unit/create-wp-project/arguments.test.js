8203b76d8917d4cac95b7e9abe5f13ac2ea788b4
Ivan Grginov <ivan.grginov@infinum.hr>
2021-01-29 17:02:45 +0100

    Remove unused tests

--- tests/unit/create-wp-project/arguments.test.js
+++ tests/unit/create-wp-project/arguments.test.js
@@ -21,23 +21,3 @@ it('tests NAMESPACE argument build method', async() => {
   expect(namespace.buildFrom.how('baNana a')).toBe('Banana_A');
   expect(namespace.buildFrom.how('#2a24 ?-.,b_ca#,')).toBe('2a24_B_Ca');
 });
-
-it('tests PREFIX argument build method', async() => {
-  const { prefix } = scriptArguments;
-  expect(prefix.buildFrom.how('banana')).toBe('BAN');
-  expect(prefix.buildFrom.how('Test name 1')).toBe('TN1');
-  expect(prefix.buildFrom.how('TEST name ab')).toBe('TNA');
-  expect(prefix.buildFrom.how('testName a')).toBe('TA');
-});
-
-it('tests ENV argument build method', async() => {
-  const { env, prefix } = scriptArguments;
-  expect(env.buildFrom.how(prefix.buildFrom.how('banana'))).toBe('BAN_ENV');
-  expect(env.buildFrom.how(prefix.buildFrom.how('TEST name ab'))).toBe('TNA_ENV');
-});
-
-it('tests PROJECT_PREFIX argument build method', async() => {
-  const { projectPrefix } = scriptArguments;
-  expect(projectPrefix.buildFrom.how('TNA')).toBe('tna');
-  expect(projectPrefix.buildFrom.how('TA1')).toBe('ta1');
-});
