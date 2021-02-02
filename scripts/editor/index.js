5bc5402a1c4c5fe3a6ee1d3e5b07fc3155d2b76b
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-02-02 21:36:43 +0100

    adding icons to storybook

--- scripts/editor/index.js
+++ scripts/editor/index.js
@@ -2,8 +2,6 @@ export { getActions } from './get-actions';
 export { getPaletteColors } from './get-palette-colors';
 export { registerBlocks, registerVariations } from './register-blocks';
 export { ucfirst } from './ucfirst';
-export { icons } from './icons';
+export { icons } from './icons/icons';
 export { overrideInnerBlockAttributes, overrideInnerBlockSimpleWrapperAttributes } from './override-inner-block-attributes';
 export { getOptionColors } from './get-option-colors';
-
-export { yoastSeo } from './yoast-seo';
