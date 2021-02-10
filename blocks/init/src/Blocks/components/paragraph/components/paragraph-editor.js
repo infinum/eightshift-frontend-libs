fdca99079369d17ef107da3c70cc84c504a032e2
Goran Alković <goran.alkovic@infinum.hr>
2021-02-10 10:30:52 +0100

    import from new file and fix namespace

--- blocks/init/src/Blocks/components/paragraph/components/paragraph-editor.js
+++ blocks/init/src/Blocks/components/paragraph/components/paragraph-editor.js
@@ -3,8 +3,8 @@ import { Fragment } from '@wordpress/element';
 import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import { RichText } from '@wordpress/block-editor';
-import { createBlock } from '@wordpress/blocks';
 import { selector, checkAttr } from '@eightshift/frontend-libs/scripts/helpers';
+import { pasteInto } from './paste-handling';
 
 import manifest from './../manifest.json';
 
@@ -19,7 +19,7 @@ export const ParagraphEditor = (attributes) => {
 		onReplace,
 		onRemove,
 		blockClass,
-		placeholder = __('Add Content', 'Tema'),
+		placeholder = __('Add Content', 'eightshift-frontend-libs'),
 
 		paragraphUse = checkAttr('paragraphUse', attributes, manifest, componentName),
 
@@ -29,8 +29,6 @@ export const ParagraphEditor = (attributes) => {
 		paragraphAlign = checkAttr('paragraphAlign', attributes, manifest, componentName),
 	} = attributes;
 
-	const allowedTags = manifest.pasteAllowTags ?? ['strong', 'b', 'i', 'em', 'span'];
-
 	const paragraphClass = classnames([
 		componentClass,
 		selector(paragraphColor, componentClass, 'color', paragraphColor),
@@ -38,134 +36,6 @@ export const ParagraphEditor = (attributes) => {
 		selector(paragraphAlign, componentClass, 'align', paragraphAlign),
 		selector(blockClass, blockClass, selectorClass),
 	]);
-
-	/**
-	 * Returns string parsed as nodes, ready for inserting into RichText.
-	 *
-	 * @param {string} inputText HTML string for parsing.
-	 */
-	const processTag = (inputText) => {
-		// If it's only text wrap it in span for processing
-		if (!inputText.startsWith('<')) {
-			inputText = `<span>${inputText}</span>`;
-		}
-
-		let output = [];
-
-		const matches = [...inputText.matchAll(/<.+?>.+?<\/.+?>/g)];
-
-		for (const [match] of matches) {
-			const elementTag = match.match(/<.+?>/g)[0];
-			const filteredTagName = elementTag.replaceAll(/[<>/\s]+/g, '');
-			const innerText = match.replaceAll(/<.+?>/g, '');
-
-			if (filteredTagName === 'span') {
-				output = [...output, document.createTextNode(`${innerText}`)];
-			} else {
-				let newElement = document.createElement(filteredTagName);
-				newElement.appendChild(document.createTextNode(innerText));
-
-				output = [...output, newElement];
-			}
-		}
-
-		return output;
-	}
-
-	/**
-	 * Process content and paste in into blocks smartly.
-	 *
-	 * @param {string} textFromClipboard Text retrieved from clipboard.
-	 */
-	const handlePaste = async (textFromClipboard) => {
-		// Split by newLine, discard empty lines
-		const parsedParagraphs = textFromClipboard.split('\n').filter((line) => line.length > 0);
-
-		// If something is selected replace it...
-		if (window.getSelection) {
-			const selection = window.getSelection();
-
-			let selectedRange = selection.getRangeAt(0);
-
-			// Process elements into nodes
-			const processedElements = processTag(parsedParagraphs[0]);
-
-			// Remove current text
-			if (!selectedRange.collapsed) {
-				selectedRange.deleteContents();
-			}
-
-			// Insert them one by one and keep spaces
-			for (const element of processedElements) {
-				selectedRange = selectedRange.cloneRange();
-				selectedRange.insertNode(element);
-				selectedRange.setStartAfter(element);
-				selectedRange.collapse(true);
-			}
-		}
-		// ...else append at the end
-		else {
-			setAttributes({ [`${componentName}Content`]: `${paragraphContent}${parsedParagraphs[0]}` });
-		}
-
-		for (const paragraph of parsedParagraphs.slice(1)) {
-
-			// Create a new block with the same attributes, but different content
-			const block = createBlock(`eightshift-boilerplate/${componentName}`, {
-				...attributes,
-				[`${componentName}Content`]: paragraph
-			});
-
-			const currentBlockClientId = wp.data.select('core/block-editor').getSelectedBlockClientId();
-			
-			const blockIndex = wp.data.select('core/block-editor').getBlockIndex(currentBlockClientId);
-
-			// Insert the block
-			wp.data.dispatch('core/block-editor').insertBlock(block, blockIndex + 1);
-		}
-	}
-
-	/**
-	 * Paste event handler.
-	 *
-	 * @param {Event} event Passed event parameters.
-	 */
-	const pasteInto = (event) => {
-		event.preventDefault();
-		const [copiedData, copiedHtml] = event.clipboardData.items;
-
-		// If HTML content is in clipboard, process that...
-		if (copiedHtml) {
-			copiedHtml.getAsString(async (inputText) => {
-				// Get all tags from the text
-				const allTags = [...inputText.matchAll(/<.+?>/g)];
-
-				// Filter out attributes and disallowed tags,
-				// make <p> and <br> elements give newline
-				for (const tag of allTags) {
-					const tagInnardsRegex = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|\s*\/?[>"']))+.)["']?/g;
-					const parsedTag = tag[0].replace(tagInnardsRegex, '');
-					const filteredTagName = parsedTag.replaceAll(/[<>/\s]+/g, '');
-
-					if (allowedTags.includes(filteredTagName)) {
-						inputText = inputText.replace(tag[0], parsedTag).replace(' >', '>');
-					}
-					else if ((filteredTagName === 'p' && !tag[0].includes('/')) || filteredTagName === 'br') {
-						inputText = inputText.replace(tag[0], '\n');
-					}
-					else {
-						inputText = inputText.replace(tag[0], '');
-					}
-				}
-
-				handlePaste(inputText);
-			});
-		}
-		// ...else handle plaintext
-		else if (copiedData) {
-			copiedData.getAsString(handlePaste);
-		}
-	};
 
 	return (
 		<Fragment>
@@ -183,7 +53,7 @@ export const ParagraphEditor = (attributes) => {
 					onMerge={mergeBlocks}
 					onReplace={onReplace}
 					onRemove={onRemove}
-					onPaste={pasteInto}
+					onPaste={(event) => pasteInto(event, attributes, setAttributes)}
 					deleteEnter={true}
 				/>
 			}
