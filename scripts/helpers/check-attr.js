7a9276dd0566fe3cb4857c01ef10cb4f4635f29c
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-03-17 00:25:35 +0100

    updating helper

--- scripts/helpers/check-attr.js
+++ scripts/helpers/check-attr.js
@@ -2,24 +2,25 @@
  * Check if attribute exist in attributes list and add default value if not.
  *
  * @param {string} key Key to check.
- * @param {array}  attributes Array of attributes.
- * @param {object}  manifest Array of default attributes from manifest.json.
+ * @param {array} attributes Array of attributes.
+ * @param {object} manifest Array of default attributes from manifest.json.
  * @param {string} componentName The real component name.
+ * @param {string} setType Override manifest key check and manually set default value.
  *
  * @return mixed
  */
-export const checkAttr = (key, attributes, manifest, componentName = '') => {
+export const checkAttr = (key, attributes, manifest, componentName = '', setType = '') => {
 
 	if (Object.prototype.hasOwnProperty.call(attributes, key)) {
 		return attributes[key];
 	} else {
 		const manifestKey = manifest.attributes[key];
 
-		if (manifestKey === 'undefined') {
+		if (manifestKey === 'undefined' && ! setType) {
 			throw Error(`${key} key does not exist in the ${componentName} component. Please check your implementation.`);
 		}
 
-		const defaultType = manifestKey.type;
+		const defaultType = manifestKey.type && setType;
 
 		let defaultValue;
 
