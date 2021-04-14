29cb8fa2285ecbfc2f262b573a8146f51c8ec511
Ivan Grginov <ivan.grginov@infinum.com>
2021-04-14 11:11:28 +0200

    Fix lint warnings

--- webpack/base.js
+++ webpack/base.js
@@ -47,7 +47,7 @@ module.exports = (options) => {
 	if (!options.overrides.includes('DependencyExtractionWebpackPlugin')) {
 		plugins.push(new DependencyExtractionWebpackPlugin({
 			outputFormat: 'json',
-			requestToExternal: function ( request ) {
+			requestToExternal: function ( request ) { // eslint-disable-line consistent-return
 				if ( request === '@wordpress/dom-ready' ) {
 					return '';
 				}
