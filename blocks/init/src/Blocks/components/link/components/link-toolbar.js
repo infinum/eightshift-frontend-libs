b8a5977c48e06d0cdea821dc3ec9d3d4ffd89613
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2021-04-15 17:48:09 +0200

    move link link option from options to toolbar

--- blocks/init/src/Blocks/components/link/components/link-toolbar.js
+++ blocks/init/src/Blocks/components/link/components/link-toolbar.js
@@ -3,6 +3,8 @@ import { __, sprintf } from '@wordpress/i18n';
 import { AlignmentToolbar } from '@wordpress/block-editor';
 import { checkAttr } from '@eightshift/frontend-libs/scripts/helpers';
 import { getOptions } from '@eightshift/frontend-libs/scripts/editor';
+import { LinkToolbarButton } from '@eightshift/frontend-libs/scripts/components';
+import { useRef } from '@wordpress/element';
 import manifest from '../manifest.json';
 
 export const LinkToolbar = (attributes) => {
@@ -16,15 +18,19 @@ export const LinkToolbar = (attributes) => {
 		linkShowControls = true,
 
 		linkUse = checkAttr('linkUse', attributes, manifest, componentName),
-
 		linkAlign = checkAttr('linkAlign', attributes, manifest, componentName),
+		linkIsNewTab = checkAttr('linkIsNewTab', attributes, manifest, componentName),
+		linkUrl = checkAttr('linkUrl', attributes, manifest, componentName),
 
 		showLinkAlign = true,
+		showLinkUrl = true,
 	} = attributes;
 
 	if (!linkShowControls) {
 		return null;
 	}
+
+	const ref = useRef();
 
 	return (
 		<>
@@ -36,6 +42,18 @@ export const LinkToolbar = (attributes) => {
 							options={getOptions(manifest, componentName, 'align', options)}
 							label={sprintf(__('%s text align', 'eightshift-frontend-libs'), label)}
 							onChange={(value) => setAttributes({ [`${componentName}Align`]: value })}
+						/>
+					}
+
+					{showLinkUrl &&
+						<LinkToolbarButton
+							componentName={componentName}
+							url={linkUrl}
+							opensInNewTab={linkIsNewTab}
+							setAttributes={setAttributes}
+							anchorRef={ref}
+							title={label}
+							textDomain={'eightshift-frontend-libs'}
 						/>
 					}
 				</>
