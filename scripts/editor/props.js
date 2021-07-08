a8b62282c27037c10ada9af467be3067adb59d19
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-07-08 21:58:18 +0200

    adding manual options to the props helper

--- scripts/editor/props.js
+++ scripts/editor/props.js
@@ -3,12 +3,13 @@ import _ from 'lodash';
 /**
  * Output only attributes that are used in the component and remove everything else.
  *
- * @param {string}  newName    - *New* key to use to rename attributes.
- * @param {object}  attributes - Attributes from the block/component.
+ * @param {string} newName     - *New* key to use to rename attributes.
+ * @param {object} attributes  - Attributes from the block/component.
+ * @param {object} [manual={}] - Object of attributes to change key and merge to the original output.
  * 
  * @returns object
  */
-export const props = (newName, attributes) => {
+export const props = (newName, attributes, manual = {}) => {
 
 	const output = {};
 
@@ -43,6 +44,24 @@ export const props = (newName, attributes) => {
 		if (key.startsWith(output['prefix'])) {
 			Object.assign(output, {[key]: value});
 		}
+	}
+
+	// Check if you have manual object and prepare the attribute keys and merge them with the original attributes for output.
+	if (!_.isEmpty(manual)) {
+		for (let [key, value] of Object.entries(manual)) {
+
+			// Remove the current component name from the attribute name.
+			const newKey = key.replace(`${_.lowerFirst(_.camelCase(newName))}`, '');
+
+			// Remove the old key.
+			delete manual[key];
+
+			// Add new key to the output with prepared attribute name.
+			Object.assign(manual, {[`${output['prefix']}${newKey}`]: value})
+		}
+
+		// Merge manual and output objects to one.
+		Object.assign(output, manual);
 	}
 
 	// Return the original attribute for optimization purposes.
