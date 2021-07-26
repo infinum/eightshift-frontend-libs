56ca6a21aee86267e7d4e1a0501fa67d7f72c6ee
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2021-07-26 14:54:45 +0200

    improve loader

--- blocks/init/src/Blocks/components/loader/components/loader-editor.js
+++ blocks/init/src/Blocks/components/loader/components/loader-editor.js
@@ -24,17 +24,24 @@ export const LoaderEditor = (attributes) => {
 		selector(loaderUseOverlay, componentClass, '', 'use-overlay'),
 	);
 
+	if (!loaderUse) {
+		return null;
+	}
+
 	return (
-		<>
-			{loaderUse &&
-				<div className={loaderClass}>
-					<div className={`${componentClass}__load`}>
-						<div className={`${componentClass}__item ${componentClass}__item--1`}></div>
-						<div className={`${componentClass}__item ${componentClass}__item--2`}></div>
-						<div className={`${componentClass}__item ${componentClass}__item--3`}></div>
-					</div>
-				</div>
-			}
-		</>
+		<div className={loaderClass}>
+			<svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" >
+				<g fill="none" fillRule="evenodd" strokeWidth="2">
+					<circle cx="22" cy="22" r="1" stroke="var(--loader-color-1, currentColor)">
+						<animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite" />
+						<animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite" />
+					</circle>
+					<circle cx="22" cy="22" r="1" stroke="var(--loader-color-2, currentColor)">
+						<animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite" />
+						<animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite" />
+					</circle>
+				</g>
+			</svg>
+		</div>
 	);
 };
