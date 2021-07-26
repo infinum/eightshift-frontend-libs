110fdce91324345af0252f7b90178136c04f7f62
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2021-07-26 19:34:22 +0200

    add 'standalone' option to IconLabel

--- scripts/components/icon-label/icon-label.js
+++ scripts/components/icon-label/icon-label.js
@@ -4,12 +4,24 @@ import { Icon } from '@wordpress/components';
 /**
  * A simple icon-label combo for streamlined components.
  * 
- * @param {React.Component} props.icon  - Icon to display.
- * @param {React.Component} props.label - Label to display.
+ * @param {React.Component} props.icon       - Icon to display.
+ * @param {React.Component} props.icon       - Icon to display.
+ * @param {boolean} [props.standalone=false] - If `true` label is wrapped in div so it can be used by itself.
  */
-export const IconLabel = ({ label, icon }) => (
-	<>
-		<Icon icon={icon} />
-		{label}
-	</>
-);
+export const IconLabel = ({ label, icon, standalone = false }) => {
+	if (standalone) {
+		return (
+			<div className='es-label-flex'>
+				<Icon icon={icon} />
+				{label}
+			</div>
+		);
+	}
+
+	return (
+		<>
+			<Icon icon={icon} />
+			{label}
+		</>
+	);
+};
