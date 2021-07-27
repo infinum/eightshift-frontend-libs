71be64b7f8bb032e87b0c765f6a39441b667d4b4
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2021-07-27 14:46:18 +0200

    remove redundant eslint disable

--- blocks/init/src/Blocks/components/icon/components/icon-options.js
+++ blocks/init/src/Blocks/components/icon/components/icon-options.js
@@ -1,4 +1,4 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { checkAttr, getAttrKey, getOption, CustomSelect, ComponentUseToggle } from '@eightshift/frontend-libs/scripts';
 import { components } from 'react-select';
