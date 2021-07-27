d01a72ab95da14009c7440f79b890836d4a5a9e4
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2021-07-27 16:44:27 +0200

    Merge TabsPanel component with TabsEditor

--- blocks/init/src/Blocks/custom/tabs/components/tabs-editor.js
+++ blocks/init/src/Blocks/custom/tabs/components/tabs-editor.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import { InnerBlocks } from '@wordpress/block-editor';
-import { checkAttr, getUnique } from '@eightshift/frontend-libs/scripts';
-import { TabsPanel } from './tabs-panel.js';
+import { checkAttr, getUnique, selector } from '@eightshift/frontend-libs/scripts';
 import manifest from './../manifest.json';
 
 export const TabsEditor = (props) => {
-	const {attributes, clientId, inner} = props;
+	const {
+		attributes,
+		parentClientId,
+		inner,
+		setAttributes
+	} = props;
 	const tabsAllowedBlocks = checkAttr('tabsAllowedBlocks', attributes, manifest);
 	const newTabTemplate = [['eightshift-boilerplate/tab', {
 		'tabTitle': __('New tab', 'eightshift-frontend-libs'),
 		'tabId': getUnique(),
 	}]];
+	const activeTab = checkAttr('tabsCurrentlyActiveTabId', attributes, manifest, true);
+
+	const tabItems = inner.map((block) => 
+		{
+			const classes = classnames([
+				selector(manifest.tabPanelItemElement, manifest.blockClass, manifest.tabPanelItemElement),
+				activeTab === block.tabId ? manifest.tabPanelItemActiveClass : ''
+			])
+			return (
+				<li
+					key={block.tabId}
+					className={classes}
+				>
+					<button
+						onClick={() => {
+							wp.data.dispatch('core/block-editor').updateBlock(parentClientId, {'tabsCurrentlyActiveTabId': block.tabId});
+							setAttributes({"tabsCurrentlyActiveTabId": block.tabId})
+						}}
+						className={selector(manifest.tabPanelItemButtonElement, manifest.blockClass, manifest.tabPanelItemButtonElement)}
+					>
+						{block.tabTitle}
+					</button>
+				</li>
+			);
+		}
+	);
+
+
 	
 	return (
 		<>
-			<TabsPanel
-				childBlocks = {inner}
-				activeTab = {checkAttr('tabsCurrentlyActiveTabId', attributes, manifest, true)}
-				parentClientId = {clientId}
-				{...props}
-			/>
+			<ul className="block-tabpanel">
+				{tabItems}
+			</ul>
+			
 			<InnerBlocks
 				allowedBlocks={(typeof tabsAllowedBlocks === 'undefined') || tabsAllowedBlocks}
 				orientation="horizontal"
