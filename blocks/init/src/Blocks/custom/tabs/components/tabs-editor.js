ce481cbebcff8943cff31f453896cbfbf3348259
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2021-08-02 12:08:20 +0200

    Move tabs-panel to tabs-editor, improve editing UX and code style
    This commit moves the tabs panel component into tabs-editor and improves the user experience by making tab titles editable in the tabs panel and moving the inserter button into the tabs panel.
    
    It also changes the way classnames are defined from "being fetched from manifest" - they now use the block class attribute, while element classes are defined in file. The tab template has also been improved, along with general code style.

--- blocks/init/src/Blocks/custom/tabs/components/tabs-editor.js
+++ blocks/init/src/Blocks/custom/tabs/components/tabs-editor.js
@@ -1,62 +1,98 @@
 import React from 'react';
 import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
-import { InnerBlocks } from '@wordpress/block-editor';
-import { checkAttr, getUnique, selector } from '@eightshift/frontend-libs/scripts';
+import { InnerBlocks, RichText } from '@wordpress/block-editor';
+import { createBlock } from '@wordpress/blocks';
+import { Button } from '@wordpress/components';
+import { checkAttr, selector, icons } from '@eightshift/frontend-libs/scripts';
 import manifest from './../manifest.json';
 
 export const TabsEditor = (props) => {
 	const {
 		attributes,
-		parentClientId,
-		inner,
-		setAttributes
+		clientId,
+		inner
 	} = props;
+
+	const {
+		blockClass,
+		activeTab,
+		setActiveTab
+	} = attributes;
+
+	const tabPanelItemElement = 'panel-item';
+	const tabPanelItemButtonElement = `${tabPanelItemElement}-button`;
+	const tabPanelItemActiveClass = 'is-active';
+	const tabPanelAddTabButtonElement = `${tabPanelItemButtonElement}-add`;
 	const tabsAllowedBlocks = checkAttr('tabsAllowedBlocks', attributes, manifest);
-	const newTabTemplate = [['eightshift-boilerplate/tab', {
-		'tabTitle': __('New tab', 'eightshift-frontend-libs'),
-		'tabId': getUnique(),
-	}]];
-	const activeTab = checkAttr('tabsCurrentlyActiveTabId', attributes, manifest, true);
 
-	const tabItems = inner.map((block) => 
-		{
-			const classes = classnames([
-				selector(manifest.tabPanelItemElement, manifest.blockClass, manifest.tabPanelItemElement),
-				activeTab === block.tabId ? manifest.tabPanelItemActiveClass : ''
-			])
-			return (
-				<li
-					key={block.tabId}
-					className={classes}
+	const newTabTemplate = [
+		['eightshift-boilerplate/tab',
+			{},
+			[
+				['eightshift-boilerplate/paragraph']
+			]
+		]
+	];
+
+	const tabItems = inner.map((block) => {
+		const classes = classnames([
+			selector(tabPanelItemElement, blockClass, tabPanelItemElement),
+			activeTab === block.tabId ? tabPanelItemActiveClass : ''
+		])
+
+		return (
+			<li
+				key={block.tabId}
+				className={classes}
+			>
+				<button
+					onClick={() => {
+						setActiveTab(block.tabId);
+					}}
+					className={selector(tabPanelItemButtonElement, blockClass, tabPanelItemButtonElement)}
 				>
-					<button
-						onClick={() => {
-							wp.data.dispatch('core/block-editor').updateBlock(parentClientId, {'tabsCurrentlyActiveTabId': block.tabId});
-							setAttributes({"tabsCurrentlyActiveTabId": block.tabId})
-						}}
-						className={selector(manifest.tabPanelItemButtonElement, manifest.blockClass, manifest.tabPanelItemButtonElement)}
-					>
-						{block.tabTitle}
-					</button>
-				</li>
-			);
-		}
-	);
+					<RichText
+						tagname=""
+						withoutInteractiveFormatting={true}
+						value={block.tabTitle}
+						onChange={(title) => wp.data.dispatch('core/block-editor').updateBlockAttributes(block.clientId, { 'tabTitle': title })}
+						placeholder={ __( 'New tab name...' , 'eightshift-frontend-libs') }
+					/>
+				</button>
+			</li>
+		);
+	});
 
+	const addInnerBlock = () => {
+		const innerCount = inner?.length ?? 0;
+		const block = createBlock('eightshift-boilerplate/tab');
+		wp.data.dispatch('core/block-editor').insertBlock(block, innerCount, clientId);
+	};
 
 	
 	return (
-		<>
+		<div className={blockClass}>
 			<ul className="block-tabpanel">
 				{tabItems}
+				<li className={selector(tabPanelItemElement, blockClass, tabPanelItemElement)}>
+					<Button
+						isSecondary
+						isSmall
+						onClick={addInnerBlock}
+						icon={icons.add}
+						className={selector(tabPanelAddTabButtonElement, blockClass, tabPanelAddTabButtonElement)}
+						label={__('Add tab', 'eightshift-frontend-libs')}
+					/>
+				</li>
 			</ul>
 			
 			<InnerBlocks
 				allowedBlocks={(typeof tabsAllowedBlocks === 'undefined') || tabsAllowedBlocks}
 				orientation="horizontal"
 				template={newTabTemplate}
+				renderAppender={() => null}
 			/>
-		</>
+		</div>
 	);
 }
