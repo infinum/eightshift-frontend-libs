dfe5e7448263b7424428581aded9c061b06e33dc
Ivan Ružević <ivan.ruzevic@infinum.hr>
2021-08-12 14:34:57 +0200

    Update scripts/editor/get-actions.js
    Co-authored-by: Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>

--- scripts/editor/get-actions.js
+++ scripts/editor/get-actions.js
@@ -138,7 +138,7 @@ const mediaPropsAction = (setAttributes, key) => {
  * @param {object} props    - Block props so we can get `setAttributes` method.
  * @param {object} manifest - Block manifest.json so we can get all attributes.
  *
- * @deprecated We stopped using getActions helper and covered to the native way of writing setAttributes
+ * @deprecated This helper is not used anymore. Write regular `setAttributes` instead.
  *
  * @returns {object}
  */
