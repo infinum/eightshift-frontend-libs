66af42c6dfe5bbe7d2c409fa1d7f17e0bf1dbb62
Simun Ivanac <simun.ivanac@infinum.com>
2021-08-23 13:35:39 +0200

    add frontend html

--- blocks/init/src/Blocks/components/modal/modal.php
+++ blocks/init/src/Blocks/components/modal/modal.php
@@ -25,6 +25,10 @@ $blockClass = $attributes['blockClass'] ?? '';
 $selectorClass = $attributes['selectorClass'] ?? $componentClass;
 $componentJsClass = $manifest['componentJsClass'] ?? '';
 
+$modalExitButton = Components::checkAttr('modalExitButton', $attributes, $manifest);
+$modalContent = Components::checkAttr('modalContent', $attributes, $manifest);
+$modalId = Components::checkAttr('modalId', $attributes, $manifest);
+
 $modalClass = Components::classnames([
 	Components::selector($componentClass, $componentClass),
 	Components::selector($blockClass, $blockClass, $selectorClass),
@@ -32,14 +36,48 @@ $modalClass = Components::classnames([
 	Components::selector($componentJsClass, $componentJsClass),
 ]);
 
+$modalOverlayClass = Components::selector($componentClass, $componentClass, 'overlay');
+$modalDialogClass = Components::selector($componentClass, $componentClass, 'dialog');
+$modalContentClass = Components::selector($componentClass, $componentClass, 'content');
+$modalExitButtonClass = Components::selector($componentClass, $componentClass, 'close-button');
+
 ?>
 
 <div
 	class="<?php echo \esc_attr($modalClass); ?>"
 	data-id="<?php echo \esc_attr($unique); ?>"
+	id="<?php echo \esc_attr($modalId); ?>"
+	aria-hidden="true"
 >
 	<?php
 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		echo Components::outputCssVariables($attributes, $manifest, $unique, $globalManifest);
 	?>
+
+	<div
+		class="<?php echo \esc_attr($modalOverlayClass); ?>"
+		tabIndex="-1"
+		data-micromodal-close
+	>
+		<div
+			class="<?php echo \esc_attr($modalDialogClass); ?>"
+			role="dialog"
+			aria-modal="true"
+		>
+			<?php if ($modalExitButton) { ?>
+				<button
+					class="<?php echo \esc_attr($modalExitButtonClass); ?>"
+					aria-label="Close modal"
+					data-micromodal-close
+				>X</button>
+			<?php } ?>
+
+			<div class="<?php echo \esc_attr($modalContentClass); ?>">
+				<?php
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					echo $modalContent; // @phpstan-ignore-line
+				?>
+			</div>
+		</div>
+	</div>
 </div>
