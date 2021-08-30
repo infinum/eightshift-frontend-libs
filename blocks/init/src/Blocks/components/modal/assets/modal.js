264d6d365e39329c7e97b940a4c9b7ff06c7a2ad
Simun Ivanac <simun.ivanac@infinum.com>
2021-08-30 11:11:11 +0200

    remove unused options from micromodal

--- blocks/init/src/Blocks/components/modal/assets/modal.js
+++ blocks/init/src/Blocks/components/modal/assets/modal.js
@@ -8,18 +8,10 @@ export class Modal {
 
 	init() {
 		MicroModal.init({
-			onShow: modal => {
-				modal.classList.add(this.jsClass);
-			},
-			onClose: modal => {
-				modal.classList.remove(this.jsClass);
-			},
 			openClass: this.openClass,
 			openTrigger: `data-${this.jsClass}-open`,
 			closeTrigger: `data-${this.jsClass}-close`,
 			disableScroll: true,
-			awaitOpenAnimation: true,
-			awaitCloseAnimation: true
 		});
 	}
 }
