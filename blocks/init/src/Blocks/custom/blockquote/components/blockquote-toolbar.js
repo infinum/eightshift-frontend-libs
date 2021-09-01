acb074509736495d49f1dee5878432a0ad966b09
Simun Ivanac <simun.ivanac@infinum.com>
2021-09-01 12:52:20 +0200

    set block alignment option

--- blocks/init/src/Blocks/custom/blockquote/components/blockquote-toolbar.js
+++ blocks/init/src/Blocks/custom/blockquote/components/blockquote-toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
+import { checkAttr, getAttrKey, getOption, AlignmentToolbar, AlignmentToolbarType } from '@eightshift/frontend-libs/scripts';
 import manifest from './../manifest.json';
 
 export const BlockquoteToolbar = ({ attributes, setAttributes }) => {
@@ -7,8 +8,15 @@ export const BlockquoteToolbar = ({ attributes, setAttributes }) => {
 		title: manifestTitle,
 	} = manifest;
 
+	const blockquoteAlign = checkAttr('blockquoteAlign', attributes, manifest);
+
 	return (
-		<>
-		</>
+		<AlignmentToolbar
+			value={blockquoteAlign}
+			options={getOption('blockquoteAlign', attributes, manifest)}
+			label={sprintf(__('%s align', 'eightshift-frontend-libs'), manifestTitle)}
+			onChange={(value) => setAttributes({ [getAttrKey('blockquoteAlign', attributes, manifest)]: value })}
+			type={AlignmentToolbarType.HORIZONTAL}
+		/>
 	);
 };
