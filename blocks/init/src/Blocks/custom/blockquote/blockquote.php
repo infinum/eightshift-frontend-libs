98f1c15647d9945b46e8c0725e5ac989b6fc5ccb
Simun Ivanac <simun.ivanac@infinum.com>
2021-09-01 12:46:59 +0200

    render component in block

--- blocks/init/src/Blocks/custom/blockquote/blockquote.php
+++ blocks/init/src/Blocks/custom/blockquote/blockquote.php
@@ -20,5 +20,10 @@ $unique = Components::getUnique();
 <div class="<?php echo esc_attr($blockClass); ?>" data-id="<?php echo \esc_attr($unique); ?>">
 	<?php
 	echo Components::outputCssVariables($attributes, $manifest, $unique, $globalManifest); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+	echo Components::render( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		'blockquote',
+		Components::props('blockquote', $attributes)
+	);
 	?>
 </div>
