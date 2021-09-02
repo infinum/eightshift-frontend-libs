05597d522c2b477e91075e517e5d1445d546435a
Simun Ivanac <simun.ivanac@infinum.com>
2021-09-02 11:28:21 +0200

    fix component story

--- blocks/init/src/Blocks/components/quote/docs/story.js
+++ blocks/init/src/Blocks/components/quote/docs/story.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Fragment } from '@wordpress/element';
 import { getExample, props, getOptions } from '@eightshift/frontend-libs/scripts';
 import readme from './readme.mdx';
 import manifest from './../manifest.json';
 import { QuoteEditor } from '../components/quote-editor';
 import { QuoteOptions } from '../components/quote-options';
+import { QuoteToolbar } from '../components/quote-toolbar';
 
 export default {
 	title: `Components/${manifest.title}`,
@@ -29,34 +29,10 @@ export const options = () => (
 	/>
 );
 
-export const size = () => (
-	<Fragment>
-		{manifest.options.quoteSize.map((values, index) => (
-			<Fragment key={index}>
-				<QuoteEditor
-					{...props('quote', attributes, {
-						quoteContent: values.label,
-						quoteSize: values.value,
-					})}
-				/>
-				<br /><br />
-			</Fragment>
-		))}
-	</Fragment>
-);
-
-export const color = () => (
-	<Fragment>
-		{manifest.options.quoteColor.map((values, index) => (
-			<Fragment key={index}>
-				<QuoteEditor
-					{...props('quote', attributes, {
-						quoteContent: values,
-						quoteColor: values,
-					})}
-				/>
-				<br /><br />
-			</Fragment>
-		))}
-	</Fragment>
+export const toolbar = () => (
+	<QuoteToolbar
+		{...props('quote', attributes, {
+			options: getOptions(attributes, manifest),
+		})}
+	/>
 );
