17dcbb89a16e0a6ad59a3326a5903582bddebe41
Milorad Jekic <miloradjekic@outlook.com>
2021-11-09 16:47:25 +0100

    Fix typos, delete fragments, simplified copying function

--- blocks/init/src/Blocks/wrapper/components/wrapper-toolbar.js
+++ blocks/init/src/Blocks/wrapper/components/wrapper-toolbar.js
@@ -10,7 +10,8 @@ export const WrapperToolbar = ({attributes, setAttributes}) => {
 
         const copiedWrapperAttributes = Object.keys(attributes).filter((key) => key.includes('wrapper'))
         .reduce((cur, key) => { 
-            return Object.assign(cur, { [key]: attributes[key] });
+            cur[key] = attributes[key];
+            return cur;
         }, {});
 
         localStorage.setItem('esCopiedWrapperAttributes', JSON.stringify(copiedWrapperAttributes));
@@ -24,29 +25,25 @@ export const WrapperToolbar = ({attributes, setAttributes}) => {
     };
 
 	return (
-		<>
-            <DropdownMenu 
-                icon={icons.wrapperConfig}
-                label={__('Copy/Paste wrapper attributes', 'eighsfhit-frontend-libs')}
-                className="components-toolbar es-toolbar-icon-24"
-            >
-                { ( { onClose } ) => (
-                    <>
-                        <MenuGroup>
-                            <MenuItem onClick={ () => {copyAttributes(onClose);} } icon={icons.copy}>
-                                <button className="components-button components-dropdown-menu__toggle has-icon">
-                                    {__('Copy Wrapper Attributes', 'eighsfhit-frontend-libs')}
-                                </button>
-                            </MenuItem>
-                            <MenuItem onClick={ () => {pasteAttributes(onClose);} } icon={icons.paste}>
-                                <button className="components-button components-dropdown-menu__toggle has-icon">
-                                    {__('Paste Wrapper Attributes', 'eighsfhit-frontend-libs')}
-                                </button>
-                            </MenuItem>
-                        </MenuGroup>
-                    </>
-                ) }
-            </DropdownMenu>
-        </>
+        <DropdownMenu 
+            icon={icons.wrapperConfig}
+            label={__('Copy/paste wrapper attributes', 'eighsfhit-frontend-libs')}
+            className="components-toolbar es-toolbar-icon-24"
+        >
+            { ( { onClose } ) => (
+                <MenuGroup>
+                    <MenuItem onClick={() => copyAttributes(onClose)} icon={icons.copy}>
+                        <button className="components-button components-dropdown-menu__toggle has-icon">
+                            {__('Copy wrapper attributes', 'eighsfhit-frontend-libs')}
+                        </button>
+                    </MenuItem>
+                    <MenuItem onClick={() => pasteAttributes(onClose)} icon={icons.paste}>
+                        <button className="components-button components-dropdown-menu__toggle has-icon">
+                            {__('Paste wrapper attributes', 'eighsfhit-frontend-libs')}
+                        </button>
+                    </MenuItem>
+                </MenuGroup>
+            ) }
+        </DropdownMenu>
 	);
 };
