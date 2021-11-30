3b3655e3ffff92e0770acc03002b018aa3ad6320
Karlo Volf <karlo.volf@infinum.com>
2021-11-30 14:02:53 +0100

    add throttle helper

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -1,5 +1,6 @@
 export { cookies } from './cookies';
 export { debounce } from './debounce';
+export { throttle } from './throttle';
 export { device } from './devices';
 export { dynamicImport } from './dynamic-import';
 export { escapeString } from './escape-string';
