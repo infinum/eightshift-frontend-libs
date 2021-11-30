c91e0a07ff33a479ae82e74fb5da339a6174af6b
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2021-11-30 16:37:32 +0100

    Change charset meta tags to use only meta charset, not http-equiv as well

--- blocks/init/src/Blocks/components/head/head.php
+++ blocks/init/src/Blocks/components/head/head.php
@@ -17,16 +17,15 @@ $headName = Components::checkAttr('headName', $attributes, $manifest);
 
 ?>
 
-<meta charset="<?php echo \esc_attr($headCharset); ?>" />
+<?php if (isset($headCharset)) { ?>
+  <meta charset="<?php echo \esc_attr($headCharset); ?>" />
+<?php } ?>
 
 <!-- Responsive -->
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
 <!-- Remove IE's ability to use compatibility mode -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-<!-- Correct type -->
-<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 
 <!-- Disable phone formatting on safari -->
 <meta name="format-detection" content="telephone=no">
