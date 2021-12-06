66092666f3672c7dc968a68011688664793923e5
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2021-12-06 10:57:51 +0100

    add null-guard to custom-select selectionCache

--- scripts/components/custom-select/custom-select.js
+++ scripts/components/custom-select/custom-select.js
@@ -139,7 +139,7 @@ export const CustomSelect = (props) => {
 		}
 
 		if (simpleValue && selectedOptions && selectedOptions?.value) {
-			let selectionCache = JSON.parse(window.localStorage.getItem('es-custom-select-cache'));
+			let selectionCache = JSON.parse(window.localStorage.getItem('es-custom-select-cache') ?? '{}');
 			window.localStorage.setItem('es-custom-select-cache', JSON.stringify({ ...selectionCache, [selectedOptions.value]: selectedOptions }));
 		}
 
@@ -166,7 +166,7 @@ export const CustomSelect = (props) => {
 			return options.filter(({ value }) => value === selected);
 		}
 
-		let selectionCache = JSON.parse(window.localStorage.getItem('es-custom-select-cache'));
+		const selectionCache = JSON.parse(window?.localStorage?.getItem('es-custom-select-cache') ?? '{}');
 
 		const itemFromAsyncOptions = defaultOptions.filter(({ value }) => value === selected)[0];
 
