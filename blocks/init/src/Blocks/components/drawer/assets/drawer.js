9ffe9551d9e5984b3995720a01fb5b6e4c3ace2c
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2022-01-17 17:32:17 +0100

    Refactor drawer for better accessibility
    Removes positioning, overlay support and implements a simple full-width
    drawer instead.

--- blocks/init/src/Blocks/components/drawer/assets/drawer.js
+++ blocks/init/src/Blocks/components/drawer/assets/drawer.js
@@ -2,15 +2,9 @@ export class Drawer {
 	constructor({ drawerElement }) {
 		this.drawerElement = drawerElement;
 		this.CLASS_MENU_OPEN = 'is-menu-open';
-		this.CLASS_OVERLAY_IS_SHOWING = 'page-overlay-shown';
 		this.CLASS_NO_SCROLL = 'u-no-scroll';
 
-		const overlaySelector = drawerElement?.dataset?.overlay;
 		const triggerSelector = drawerElement?.dataset?.trigger;
-
-		if (overlaySelector) {
-			this.overlay = document.querySelector(`.${overlaySelector}`);
-		}
 
 		if (triggerSelector) {
 			this.trigger = document.querySelector(`.${triggerSelector}`);
@@ -26,29 +20,17 @@ export class Drawer {
 	}
 	
 	openMobileMenu() {
-		if (this.overlay) {
-			document.body.classList.add(this.CLASS_OVERLAY_IS_SHOWING);
-			this.trigger?.classList.add(this.CLASS_MENU_OPEN);
-		}
-
+		this.trigger?.classList.add(this.CLASS_MENU_OPEN);
 		document.body.classList.add(this.CLASS_MENU_OPEN);
 		this.preventScroll();
 	}
 	
 	closeMobileMenu() {
-		if (this.overlay) {
-			document.body.classList.remove(this.CLASS_OVERLAY_IS_SHOWING);
-			this.trigger?.classList.remove(this.CLASS_MENU_OPEN);
-		}
-
+		this.trigger?.classList.remove(this.CLASS_MENU_OPEN);
 		document.body.classList.remove(this.CLASS_MENU_OPEN);
 		this.enableScroll();
 	}
 	
-	closeMobileMenuOnOverlayClick() {
-		this.overlay?.addEventListener('click', () => this.closeMobileMenu());
-	}
-
 	init() {
 		this.trigger.addEventListener('click', () => {
 			if (document.body.classList.contains(this.CLASS_MENU_OPEN)) {
@@ -57,7 +39,6 @@ export class Drawer {
 				this.openMobileMenu();
 			}
 		});
-
-		this.closeMobileMenuOnOverlayClick();
 	}
+
 }
