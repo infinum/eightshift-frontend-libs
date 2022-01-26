ca3a1edbe7cfa886997826e72e5c8ebdad13f1f3
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-01-26 10:29:41 +0100

    update scripts/editor exports

--- scripts/editor/index.js
+++ scripts/editor/index.js
@@ -1,18 +1,38 @@
-export { getActions } from './get-actions';
-export { getPaletteColors } from './get-palette-colors';
-export { registerBlocks, registerVariations } from './register-blocks';
-export { ucfirst } from './ucfirst';
-export { icons, BlockIcon } from './icons/icons';
+// All exports are sorted in alphabetical order.
+
 export {
-	overrideInnerBlockAttributes,
-	overrideInnerBlockSimpleWrapperAttributes
-} from './override-inner-block-attributes';
-export { getOptionColors } from './get-option-colors';
-export { pasteInto } from './paste-handler';
+	icons,
+	illustrations,
+	blockIcons,
+	BlockIcon
+} from './icons/icons';
+export { getFetchWpApi } from './fetch-wp-api';
+export { getActions } from './get-actions';
 export { getOptions } from './get-options';
+export { getOptionColors } from './get-option-colors';
+export { getPaletteColors } from './get-palette-colors';
+export {
+	lockIfUndefined,
+	lockPostEditing,
+	unlockPostEditing,
+} from './lock-editing';
 export {
 	outputCssVariablesGlobal,
 	outputCssVariables,
 	getUnique
 } from './output-css-variables';
+export {
+	overrideInnerBlockAttributes,
+	overrideInnerBlockSimpleWrapperAttributes
+} from './override-inner-block-attributes';
+export { pasteInto } from './paste-handler';
 export { props } from './props';
+export {
+	getAttributes,
+	getExample,
+	getFullBlockName,
+	getFullBlockNameVariation,
+	registerBlocks,
+	registerVariations,
+} from './register-blocks';
+export { ucfirst } from './ucfirst';
