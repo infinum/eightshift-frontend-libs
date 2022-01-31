cee65ecf78606c3d3084b8a97cd32f443a9840e0
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2022-02-01 00:37:42 +0100

    updating wrapper grid helper

--- blocks/init/src/Blocks/wrapper/components/wrapper-editor.js
+++ blocks/init/src/Blocks/wrapper/components/wrapper-editor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelect } from '@wordpress/data';
 import { outputCssVariables, checkAttr } from '@eightshift/frontend-libs/scripts';
 import manifest from './../manifest.json';
 import globalManifest from './../../manifest.json';
@@ -8,16 +9,40 @@ export const WrapperEditor = ({ clientId, attributes, children }) => {
 		blockClass,
 	} = attributes;
 
+	const wrapperMainClass = 'wrapper';
+
 	const wrapperDisable = checkAttr('wrapperDisable', attributes, manifest);
 
 	if (!wrapperDisable) {
 		attributes.uniqueWrapperId = clientId;
 	}
 
+	const isEditMode = useSelect((select) => {
+		return select('core/block-editor').isNavigationMode();
+	});
+
+	const GetGridLayout = () => {
+		const items = [];
+
+		for(let i = 1; i <= globalManifest.globalVariables.maxCols; i++) {
+			items.push(<div className={`${wrapperMainClass}__grid-item`} key={i}>{i}</div>);
+		}
+
+		return (
+			<div className={`${wrapperMainClass}__grid`}>
+				{items}
+			</div>
+		);
+	};
+
 	return (
 		<>
 			{!wrapperDisable &&
 				outputCssVariables(attributes, manifest, clientId, globalManifest, blockClass)
+			}
+
+			{isEditMode &&
+				<GetGridLayout />
 			}
 
 			{children}
