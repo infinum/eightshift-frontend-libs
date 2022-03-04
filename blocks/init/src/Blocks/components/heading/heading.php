8f91f71e26136d4d6058c39e08c1a99ecbad5d0c
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2022-03-04 15:50:34 +0100

    Move outputCssVariable to heading's parent node
    Stops outputting <style> in disallowed contexts.

--- blocks/init/src/Blocks/components/heading/heading.php
+++ blocks/init/src/Blocks/components/heading/heading.php
@@ -39,10 +39,8 @@ $headingLevel = $headingLevel ? "h{$headingLevel}" : 'h2';
 $unique = Components::getUnique();
 ?>
 
+<?php echo Components::outputCssVariables($attributes, $manifest, $unique, $globalManifest); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+
 <<?php echo esc_attr($headingLevel); ?> class="<?php echo esc_attr($headingClass); ?>" data-id="<?php echo esc_attr($unique); ?>">
-
-	<?php echo Components::outputCssVariables($attributes, $manifest, $unique, $globalManifest); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-
 	<?php echo wp_kses_post($headingContent); ?>
-
 </<?php echo esc_attr($headingLevel); ?>>
