317c4035fcd304d1f72f1458bdb92e4cca694cc2
Ivan Ružević <ivan.ruzevic@infinum.hr>
2022-04-26 17:05:24 +0200

    fixing styles for deprecations

--- babel/babel.config.js
+++ babel/babel.config.js
@@ -1,14 +1,18 @@
 module.exports = {
 	presets: [
 		[
-			"@babel/preset-env",
+			'@babel/preset-env',
 			{
-				useBuiltIns: "entry",
-				corejs: 3,
+				useBuiltIns: 'entry',
+				corejs: '3',
+				modules: false,
 			}
 		],
 		[
 			"@babel/preset-react"
 		],
-	]
+	],
+	plugins: [
+		"@babel/plugin-syntax-dynamic-import"
+	],
 };
