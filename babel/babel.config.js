cd5ef266b239908d0e4c2429b42c894ede5c1d8b
Ivan Ružević <ivan.ruzevic@infinum.hr>
2022-04-26 12:48:04 +0200

    updating babel setup

--- babel/babel.config.js
+++ babel/babel.config.js
@@ -1,35 +1,8 @@
 module.exports = {
 	presets: [
-		"@wordpress/default",
-	],
-	plugins: [
-		'@babel/plugin-syntax-dynamic-import',
-		[
-			'@babel/plugin-proposal-object-rest-spread',
-			{
-				useBuiltIns: true,
-			},
-		],
-		[
-			"@babel/plugin-proposal-class-properties",
-			{
-				"loose": true
-			}
-		],
-		[
-			"@babel/plugin-proposal-private-methods",
-			{
-				"loose": true
-			}
-		],
-		[
-			"@babel/plugin-proposal-private-property-in-object",
-			{
-				"loose": true
-			}
-		],
-	],
-	env: {
-		test: {}
-	}
+		"@babel/preset-env",
+		{
+			"useBuiltIns": "entry"
+		}
+	]
 };
