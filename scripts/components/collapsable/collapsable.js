dad6a58222336b4a7bdd647d4d68691db07ae6e2
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-05-02 07:58:48 +0200

    rework collapsable component

--- scripts/components/collapsable/collapsable.js
+++ scripts/components/collapsable/collapsable.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { __ } from '@wordpress/i18n';
 import { BaseControl, Button, Animate } from '@wordpress/components';
 import { icons } from '../../../scripts';
 
@@ -27,16 +28,17 @@ export const Collapsable = ({
 
 	return (
 		<BaseControl className={componentClasses}>
-			<div className='es-collapsable-v2__trigger'>
+			<div className='es-collapsable-v2__trigger es-h-between'>
 				{label}
 
 				{showExpanderIcon &&
 					<Button
 						onClick={() => setIsOpen(!isOpen)}
-						className='es-button-icon-24 es-button-square-32 es-justify-content-center es-align-items-center'
-					>
-						{icons.caretDown}
-					</Button>
+						icon={icons.caretDown}
+						className='es-collapsable-v2__expander-button es-button-square-32 es-button-icon-24'
+						label={isOpen ? __('Show options', 'eightshift-frontend-libs') : __('Hide options', 'eightshift-frontend-libs')}
+						showTooltip
+					/>
 				}
 			</div>
 
