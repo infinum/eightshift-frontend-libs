fe0748fd9e2809be854bdaca6e4ffa9b604c80a2
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-05-03 13:27:38 +0200

    add new look option to FancyDivider

--- scripts/components/fancy-divider/fancy-divider.js
+++ scripts/components/fancy-divider/fancy-divider.js
@@ -3,16 +3,20 @@ import React from 'react';
 /**
  * A simple <hr /> replacement that draws text.
  * 
- * @param {object} props       - FancyDivider options.
- * @param {string} props.label - Label shown on the divider.
+ * @param {object} props                      - FancyDivider options.
+ * @param {string} props.label                - Label shown on the divider.
+ * @param {boolean} [props.lowercase=true]    - If `true`, the label is shown in an alternate, lowercase style.
+ * @param {string?} [props.additionalClasses] - Allows passing through extra classes.
  */
 export const FancyDivider = (props) => {
 	const {
 		label,
+		lowercase = true,
+		additionalClasses,
 	} = props;
 
 	return (
-		<span className='es-fancy-divider'>
+		<span className={['es-fancy-divider', lowercase ? 'es-fancy-divider--lowercase' : '', additionalClasses ?? '']}>
 			<span>{label}</span>
 		</span>
 	);
