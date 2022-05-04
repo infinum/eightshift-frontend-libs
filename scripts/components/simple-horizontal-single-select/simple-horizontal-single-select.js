5a90b967d4c91ab0910e64d7d54f5b8adb79dee2
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-05-04 11:33:41 +0200

    add vertical option to SimpleHSingleSelect

--- scripts/components/simple-horizontal-single-select/simple-horizontal-single-select.js
+++ scripts/components/simple-horizontal-single-select/simple-horizontal-single-select.js
@@ -5,22 +5,21 @@ import classnames from 'classnames';
 /**
  * Horizontal button option selector, inspired by WP 5.9.
  * 
- * @param {object} props                                                            - SimpleHorizontalSingleSelect options.
- * @param {React.Component?} [props.help]                                           - Help text displayed below the control.
- * @param {any} [props.value]                                                       - Current value.
- * @param {React.Component?} [props.label]                                          - Label displayed above the control.
- * @param {array<string|{label, value, icon, tooltip}>} [props.options]             - Options to show, either values or objects with {label?, value, icon?}
- * @param {function} [props.onChange]                                               - Function that is called on every value change.
- * @param {React.Component?} [props.inlineLabel]                                    - Label displayed inline with the control.
- * @param {array<{label, value}>?} [props.optionLabels]                             - If passed, these labels/icons will be used instead the ones provided with `options`. Must be passed when `options` contain just values.
- * @param {boolean} [props.iconOnly=false]                                          - If `true`, the buttons will only contain icons. If a label is also passed, it will be used for the button tooltip.
- * @param {boolean} [props.allowWrap=true]                                          - If `true` and there is more options then can fit, the buttons will wrap to the row below.
- * @param {boolean} [props.disabled=false]                                          - If `true`, the component will be disabled.
- * @param {'none'|'inset'|'offset'} [props.border='none']                           - Sets the appearance of a border around the buttons.
- * @param {'default'|'stretch'|'left'|'center'|'right'} [props.alignment='default'] - If `true` and there is more options then can fit, the buttons will wrap to the row below.
- * @param {boolean} [props.includeWpBottomSpacing=false]                            - If `true`, the component will add bottom spacing to match other Gutenberg components.
- * @param {boolean} [props.compactButtons=false]                                    - If `true`, the buttons are rendered smaller
- * @param {boolean} [props.largerIcons=false]                                       - If `true`, the icons inside of buttons are rendered larger.
+ * @param {object} props                                                                       - SimpleHorizontalSingleSelect options.
+ * @param {React.Component?} [props.help]                                                      - Help text displayed below the control.
+ * @param {any} [props.value]                                                                  - Current value.
+ * @param {React.Component?} [props.label]                                                     - Label displayed above the control.
+ * @param {array<string|{label, value, icon, tooltip}>} [props.options]                        - Options to show, either values or objects with {label?, value, icon?}
+ * @param {function} [props.onChange]                                                          - Function that is called on every value change.
+ * @param {React.Component?} [props.inlineLabel]                                               - Label displayed inline with the control.
+ * @param {array<{label, value}>?} [props.optionLabels]                                        - If passed, these labels/icons will be used instead the ones provided with `options`. Must be passed when `options` contain just values.
+ * @param {boolean} [props.iconOnly=false]                                                     - If `true`, the buttons will only contain icons. If a label is also passed, it will be used for the button tooltip.
+ * @param {boolean} [props.allowWrap=true]                                                     - If `true` and there is more options then can fit, the buttons will wrap to the row below.
+ * @param {boolean} [props.disabled=false]                                                     - If `true`, the component will be disabled.
+ * @param {'none'|'inset'|'offset'} [props.border='none']                                      - Sets the appearance of a border around the buttons.
+ * @param {'default'|'stretch'|'left'|'center'|'right'|'vertical'} [props.alignment='default'] - If `true` and there is more options then can fit, the buttons will wrap to the row below.
+ * @param {boolean} [props.includeWpBottomSpacing=false]                                       - If `true`, the component will add bottom spacing to match other Gutenberg components.
+ * @param {boolean} [props.compactButtons=false]                                               - If `true`, the buttons are rendered smaller
  */
 export const SimpleHorizontalSingleSelect = (props) => {
 	const {
@@ -38,13 +37,7 @@ export const SimpleHorizontalSingleSelect = (props) => {
 		alignment = 'default',
 		includeWpBottomSpacing = true,
 		compactButtons = false,
-		largerIcons = false,
 	} = props;
-
-	const buttonClasses = classnames([
-		iconOnly ? 'es-button-square-40' : '',
-		largerIcons ? 'es-button-icon-24' : '',
-	]);
 
 	const spacingConfig = {
 		default: {
@@ -68,6 +61,10 @@ export const SimpleHorizontalSingleSelect = (props) => {
 		},
 		stretch: {
 			justifyContent: 'space-between',
+			width: '100%',
+		},
+		vertical: {
+			flexDirection: 'column',
 			width: '100%',
 		}
 	};
@@ -109,7 +106,6 @@ export const SimpleHorizontalSingleSelect = (props) => {
 							icon={icon}
 							disabled={disabled}
 							isSmall={compactButtons}
-							className={iconOnly ? 'es-button-square-40' : ''}
 						/>
 					);
 				} else if (icon && label && iconOnly) {
@@ -123,7 +119,6 @@ export const SimpleHorizontalSingleSelect = (props) => {
 							disabled={disabled}
 							showTooltip
 							isSmall={compactButtons}
-							className={buttonClasses}
 						/>
 					);
 				} else if (icon && label) {
@@ -137,7 +132,6 @@ export const SimpleHorizontalSingleSelect = (props) => {
 							isSmall={compactButtons}
 							label={tooltip !== label ? tooltip : null}
 							showTooltip={tooltip !== label}
-							className={buttonClasses}
 						>
 							{label}
 						</Button>
@@ -153,7 +147,6 @@ export const SimpleHorizontalSingleSelect = (props) => {
 						isSmall={compactButtons}
 						label={tooltip !== label ? tooltip : null}
 						showTooltip={tooltip !== label}
-						className={buttonClasses}
 					>
 						{label}
 					</Button>
