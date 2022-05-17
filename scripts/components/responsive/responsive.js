6873c82d6bb009d922c600c0c8935e1d65638cd7
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-05-17 10:51:09 +0200

    fix Responsive doc comment and readme

--- scripts/components/responsive/responsive.js
+++ scripts/components/responsive/responsive.js
@@ -4,9 +4,11 @@ import { CompactResponsive } from '../compact-responsive/compact-responsive';
 /**
  * A component that displays options adjustable across screen sizes beautifully.
  * 
- * @param {object} props          - Heading level picker options.
+ * @param {object} props          - Responsive picker options.
  * @param {string?} [props.label] - Label displayed above the picker.
  * @param {array} props.children  - Items to show. First item is always visible, the rest are visible when the button is toggled.
+ * 
+ * @see {@link CompactResponsive} - replacement for Responsive with additional options.
  */
 export const Responsive = (props) => {
 	const {
