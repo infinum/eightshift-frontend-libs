edfe777e4786dfb436f880a6f60ac07318831a7e
Karlo Biscan <biscan.karlo@gmail.com>
2022-07-07 09:49:15 +0200

    Add tests for create-wp-project prompts

--- tests/unit/create-wp-project/basics/argument-operations.test.js
+++ tests/unit/create-wp-project/basics/argument-operations.test.js
@@ -4,72 +4,81 @@
  * @group unit
  */
 
-import inquirer from 'inquirer';
-import { log } from '../../../../setup/create-wp-project/src/basics/misc';
 
-jest.mock('chalk', () => ({
-	bgRed: jest.fn(),
-	red: jest.fn(),
-	blue: jest.fn(),
-	green: jest.fn(),
-	cyan: jest.fn(),
-	yellow: jest.fn(),
-}));
-
-const {
-	maybePrompt,
-} = require('../../../../setup/create-wp-project/src/basics/argument-operations');
 const { scriptArguments } = require('../../../../setup/create-wp-project/src/arguments');
 
 const {
-	eightshiftForbiddenKeywords,
-} = require('../../../../setup/create-wp-project/src/basics/variables');
+	maybePrompt,
+	projectNameValidator,
+} = require('../../../../setup/create-wp-project/src/basics/argument-operations');
 
-const checkProjectName = (projectName) => {
-	return eightshiftForbiddenKeywords.find((forbiddenWord) => forbiddenWord === projectName);
-};
-
-const testArgsSuccess = {
+const testArguments = {
 	projectName: 'TestTheme',
 	url: 'dev-url.test',
 	description: 'Test description',
 	noSummary: true,
 };
 
-const testArgsFail = {
-	projectName: 'final',
-	url: 'dev-url.test',
-	description: 'Test description',
-	noSummary: true,
-};
-
-let backup;
-
-test('Project name will not pass if it matches forrbiden keyword.', async () => {
-	expect.assertions(eightshiftForbiddenKeywords.length);
-
-	eightshiftForbiddenKeywords.forEach((word) => {
-		expect(checkProjectName(word)).toBe(word);
+describe('Create wp project prompts will pass', () => {
+	test('Project name validator will succeed if none of forbidden keywords is used.', async () => {
+		expect(projectNameValidator('Test Theme')).toBe(true);
+		expect(projectNameValidator('Test Theme')).not.toBe(false);
 	});
-});
-
-test('Project name will pass if it does not match any of forrbiden keywords.', async () => {
-	expect(checkProjectName('TestTheme')).toBeUndefined();
-});
-
-test('Prompt will succeed with correctly passed arguments.', async () => {
-	const output = await maybePrompt(scriptArguments, testArgsSuccess);
-
-	expect(output.projectName).toBe(testArgsSuccess.projectName);
-	expect(output.projectName).not.toBe('Boilerplate theme');
-
-});
-
-test('Prompt will fail with forbidden keyword.', async () => {
-	backup = inquirer.prompt;
-    inquirer.prompt = (questions) => Promise.resolve('My Theme');
-
-	expect(await maybePrompt(scriptArguments, testArgsFail).projectName).toBe('My Theme');
 	
-	inquirer.prompt = backup;
-});
+	test('Project name validator will fail if any of forbidden keywords is used.', async () => {
+		expect(projectNameValidator('final')).toBe(false);
+		expect(projectNameValidator('final')).not.toBe(true);
+	});
+	
+	test('Url argument will be the same as entered.', async () => {
+		const data = await maybePrompt(scriptArguments, testArguments);
+	
+		expect(data.url).toBe(testArguments.url);
+		expect(data.url).not.toBeUndefined();
+	});
+	
+	test('Description argument will be the same as entered.', async () => {
+		const data = await maybePrompt(scriptArguments, testArguments);
+	
+		expect(data.description).toBe(testArguments.description);
+		expect(data.description).not.toBeUndefined();
+	});
+	
+	test('Undefined arguments will be skipped entirely.', async () => {
+		const newArguments = {
+			testProp: 'Some text',
+			...testArguments,
+		};
+		const data = await maybePrompt(scriptArguments, newArguments);
+	
+		expect(data.testProp).toBeUndefined();
+		expect(data.testProp).not.toBeTruthy();
+	});
+	
+	test('Arguments with "skipPrompt" will skip prompt and won\'t be shown in answers.', async () => {
+		const newArguments = {
+			eightshiftBoilerplateRepo: 'eightshiftBoilerplateRepo',
+			...testArguments,
+		};
+
+		const data = await maybePrompt(scriptArguments, newArguments);
+
+		expect(data.eightshiftBoilerplateRepo).toBeUndefined();
+		expect(data.eightshiftBoilerplateRepo).not.toBeTruthy();
+	});
+	
+	test('Argument package will build correctly.', async () => {
+		const data = await maybePrompt(scriptArguments, testArguments);
+
+		expect(data.package).toBe('testtheme');
+		expect(data.package).not.toBeUndefined();
+	});
+	
+	test('Argument nameSpace will build correctly.', async () => {
+		const data = await maybePrompt(scriptArguments, testArguments);
+
+		expect(data.namespace).toBe('Testtheme');
+		expect(data.namespace).not.toBeUndefined();
+	});
+})
+
