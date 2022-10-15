3b9533eaf7a9cc9e3d0095a72b4e7e37fa78073a
Denis Žoljom <dingo-d@users.noreply.github.com>
2022-10-15 13:53:27 +0200

    Fix the project name in case of extra spaces

--- tests/unit/create-wp-project/basics/argument-operations.test.js
+++ tests/unit/create-wp-project/basics/argument-operations.test.js
@@ -24,37 +24,37 @@ describe('Create wp project prompts will pass', () => {
 		expect(projectNameValidator('Test Theme')).toBe(true);
 		expect(projectNameValidator('Test Theme')).not.toBe(false);
 	});
-	
+
 	test('Project name validator will fail if any of forbidden keywords is used.', async () => {
 		expect(projectNameValidator('final')).toBe(false);
 		expect(projectNameValidator('final')).not.toBe(true);
 	});
-	
+
 	test('Url argument will be the same as entered.', async () => {
 		const data = await maybePrompt(scriptArguments, testArguments);
-	
+
 		expect(data.url).toBe(testArguments.url);
 		expect(data.url).not.toBeUndefined();
 	});
-	
+
 	test('Description argument will be the same as entered.', async () => {
 		const data = await maybePrompt(scriptArguments, testArguments);
-	
+
 		expect(data.description).toBe(testArguments.description);
 		expect(data.description).not.toBeUndefined();
 	});
-	
+
 	test('Undefined arguments will be skipped entirely.', async () => {
 		const newArguments = {
 			testProp: 'Some text',
 			...testArguments,
 		};
 		const data = await maybePrompt(scriptArguments, newArguments);
-	
+
 		expect(data.testProp).toBeUndefined();
 		expect(data.testProp).not.toBeTruthy();
 	});
-	
+
 	test('Arguments with "skipPrompt" will skip prompt and won\'t be shown in answers.', async () => {
 		const newArguments = {
 			eightshiftBoilerplateRepo: 'eightshiftBoilerplateRepo',
@@ -66,19 +66,32 @@ describe('Create wp project prompts will pass', () => {
 		expect(data.eightshiftBoilerplateRepo).toBeUndefined();
 		expect(data.eightshiftBoilerplateRepo).not.toBeTruthy();
 	});
-	
+
 	test('Argument package will build correctly.', async () => {
 		const data = await maybePrompt(scriptArguments, testArguments);
 
 		expect(data.package).toBe('testtheme');
 		expect(data.package).not.toBeUndefined();
 	});
-	
+
 	test('Argument nameSpace will build correctly.', async () => {
 		const data = await maybePrompt(scriptArguments, testArguments);
 
 		expect(data.namespace).toBe('Testtheme');
 		expect(data.namespace).not.toBeUndefined();
+	});
+
+	test('Project name will result in a correct project package.', async () => {
+		const inputArgs = {
+			projectName: 'Test Theme ',
+			url: 'dev-url.test',
+			description: 'Test description',
+			noSummary: true,
+		};
+
+		const data = await maybePrompt(scriptArguments, inputArgs);
+
+		expect(data.package).toBe('test-theme');
 	});
 })
 
