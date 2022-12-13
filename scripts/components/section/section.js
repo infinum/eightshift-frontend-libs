ae9b918586e50bf837184bdb2fd3e5e73a9a35bf
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-13 11:43:36 +0100

    tweak section component

--- scripts/components/section/section.js
+++ scripts/components/section/section.js
@@ -7,13 +7,17 @@ import { FancyDivider } from '../fancy-divider/fancy-divider';
  * @param {object} props                           - Section options.
  * @param {string} props.label                     - Label shown on the divider.
  * @param {React.Component?} [props.icon]          - Icon shown on the divider.
+ * @param {boolean} [props.noBottomSpacing=false]  - If `true`, the default bottom spacing is removed.
  * @param {string?} [props.additionalClasses]      - Allows passing through extra classes.
  * @param {string?} [props.additionalLabelClasses] - Allows passing through extra classes to the label.
+ * @param {React.Component} props.children         - Child items that are shown when expanded.
  */
 export const Section = (props) => {
 	const {
 		label,
 		icon,
+
+		noBottomSpacing = false,
 
 		additionalClasses,
 		additionalLabelClasses,
@@ -22,10 +26,12 @@ export const Section = (props) => {
 	} = props;
 
 	return (
-		<div className={additionalClasses}>
+		<>
 			<FancyDivider label={label} icon={icon} additionalClasses={additionalLabelClasses} />
 
-			{children}
-		</div>
+			<div className={`${noBottomSpacing ? '' : 'es-mb-5'} ${additionalClasses ?? ''}`}>
+				{children}
+			</div>
+		</>
 	);
 };
