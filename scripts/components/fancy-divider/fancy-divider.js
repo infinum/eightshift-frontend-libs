9b6b123f0092f4b0143fe7422556e1f6a64472f8
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-13 09:11:58 +0100

    rework fancy divider

--- scripts/components/fancy-divider/fancy-divider.js
+++ scripts/components/fancy-divider/fancy-divider.js
@@ -1,23 +1,37 @@
+import { IconLabel } from '@eightshift/frontend-libs/scripts/components/icon-label/icon-label';
 import React from 'react';
 
 /**
- * A simple <hr /> replacement that draws text.
+ * A simple <hr /> replacement that draws text and an optional icon.
  *
  * @param {object} props                      - FancyDivider options.
  * @param {string} props.label                - Label shown on the divider.
- * @param {boolean} [props.lowercase=true]    - If `true`, the label is shown in an alternate, lowercase style.
+ * @param {React.Component?} [props.icon]     - Icon shown on the divider.
  * @param {string?} [props.additionalClasses] - Allows passing through extra classes.
  */
 export const FancyDivider = (props) => {
 	const {
 		label,
-		lowercase = true,
+		icon,
+
 		additionalClasses,
 	} = props;
 
+	if (!icon) {
+		return (
+			<div className={`es-h-spaced es-mt-0 es-mb-2.5 es-nested-p-0.75 es-nested-bg-cool-gray-450 es-nested-rounded-1.0 es-nested-color-pure-white es-color-cool-gray-500 ${additionalClasses ?? ''}`}>
+				{label}
+
+				<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
+			</div>
+		);
+	}
+
 	return (
-		<span className={`es-fancy-divider es-my-6 ${lowercase ? 'es-fancy-divider--lowercase' : ''} ${additionalClasses ?? ''}`}>
-			<span>{label}</span>
-		</span>
+		<div className={`es-h-spaced es-mt-0 es-mb-2.5 es-nested-p-0.75 es-color-cool-gray-500 ${additionalClasses ?? ''}`}>
+			<IconLabel icon={icon} label={label} standalone additionalClasses='es-nested-bg-cool-gray-450 es-nested-rounded-1.0 es-nested-color-pure-white!' />
+
+			<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
+		</div>
 	);
 };
