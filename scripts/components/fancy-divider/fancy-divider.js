209949647184d877fd7ddcb3f3ed33335a500360
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-13 12:12:00 +0100

    add subtitle option to fancydivider

--- scripts/components/fancy-divider/fancy-divider.js
+++ scripts/components/fancy-divider/fancy-divider.js
@@ -1,18 +1,20 @@
-import { IconLabel } from '@eightshift/frontend-libs/scripts/components/icon-label/icon-label';
+import { IconLabel } from '../icon-label/icon-label';
 import React from 'react';
 
 /**
  * A simple <hr /> replacement that draws text and an optional icon.
  *
  * @param {object} props                      - FancyDivider options.
+ * @param {React.Component?} [props.icon]     - If provided, icon shown on the divider.
  * @param {string} props.label                - Label shown on the divider.
- * @param {React.Component?} [props.icon]     - Icon shown on the divider.
+ * @param {React.Component?} [props.subtitle] - If provided, subtitle shown on the divider.
  * @param {string?} [props.additionalClasses] - Allows passing through extra classes.
  */
 export const FancyDivider = (props) => {
 	const {
-		label,
 		icon,
+		label,
+		subtitle,
 
 		additionalClasses,
 	} = props;
@@ -20,7 +22,11 @@ export const FancyDivider = (props) => {
 	if (!icon) {
 		return (
 			<div className={`es-h-spaced es-mt-3 es-mb-2.5 es-nested-p-0.75 es-nested-bg-cool-gray-450 es-nested-rounded-1.0 es-nested-color-pure-white es-color-cool-gray-500 ${additionalClasses ?? ''}`}>
-				{label}
+				{!subtitle && label}
+
+				{subtitle &&
+					<IconLabel label={label} subtitle={subtitle} />
+				}
 
 				<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
 			</div>
@@ -29,7 +35,7 @@ export const FancyDivider = (props) => {
 
 	return (
 		<div className={`es-h-spaced es-mt-3 es-mb-2.5 es-nested-p-0.75 es-color-cool-gray-500 ${additionalClasses ?? ''}`}>
-			<IconLabel icon={icon} label={label} standalone additionalClasses='es-nested-bg-cool-gray-450 es-nested-rounded-1.0 es-nested-color-pure-white!' />
+			<IconLabel icon={icon} label={label} subtitle={subtitle} standalone additionalClasses='es-nested-bg-cool-gray-450 es-nested-rounded-1.0 es-nested-color-pure-white!' />
 
 			<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
 		</div>
