707877ba46c87bab95daddcde6e1b01e524eb5fe
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-14 19:10:52 +0100

    add es version of wp BaseControl

---
+++ scripts/components/shared.js
@@ -0,0 +1,57 @@
+import { classnames } from '../helpers';
+import { IconLabel } from './icon-label/icon-label';
+
+/**
+ * A base component to optionally encase a component with a label and help.
+ *
+ * @param {object} props                           - Control options.
+ * @param {React.Component?} [props.icon]          - Icon to show next to the label
+ * @param {React.Component?} [props.label]         - Label to show above component.
+ * @param {React.Component?} [props.subtitle]      - Subtitle below the label.
+ * @param {React.Component?} [props.actions]       - Actions to show to the right of the label.
+ * @param {React.Component?} [props.help]          - Help to show below the control.
+ * @param {React.Component?} [props.children]      - Content to show.
+ * @param {string?} [props.additionalClasses]      - Classes to add to the control base.
+ * @param {boolean?} [props.noBottomSpacing=false] - If `true`, space below the control is removed.
+ */
+export const Control = (props) => {
+	const {
+		icon,
+		label,
+		subtitle,
+
+		actions,
+
+		help,
+
+		children,
+
+		additionalClasses,
+
+		noBottomSpacing = false,
+	} = props;
+
+	if (!children) {
+		return null;
+	}
+
+	return (
+		<div className={classnames(additionalClasses, !noBottomSpacing && 'es-mb-5')}>
+			{label &&
+				<div className={classnames('es-mb-2', actions && 'es-h-between')}>
+					<IconLabel icon={icon} label={label} subtitle={subtitle} standalone />
+
+					{actions &&
+						<div>{actions}</div>
+					}
+				</div>
+			}
+
+			{children}
+
+			{help &&
+				<div className='es-mt-1 es-text-3 es-color-cool-gray-500'>{help}</div>
+			}
+		</div>
+	);
+};
