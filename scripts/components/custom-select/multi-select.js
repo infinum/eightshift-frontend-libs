acfea998292cfc7f3f95c160c1a86b70b6e2c5df
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-14 10:49:00 +0100

    add doc comment and finish initial version of multiselect

--- scripts/components/custom-select/multi-select.js
+++ scripts/components/custom-select/multi-select.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
 import Select, { components } from 'react-select';
-import { DndContext, useDroppable } from '@dnd-kit/core';
+import { DndContext } from '@dnd-kit/core';
 import { restrictToParentElement } from '@dnd-kit/modifiers';
-import { arrayMove, SortableContext, useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+import { SortableContext } from '@dnd-kit/sortable';
 import { defaultEightshiftColorScheme, defaultEightshiftStyles } from './custom-select-style';
 import { CustomSelectDefaultClearIndicator, CustomSelectDefaultDropdownIndicator } from './custom-select-default-components';
+import { getDragEndHandler, getMultiValue, getMultiValueContainer, getMultiValueRemove } from './reorderable-select-components';
 
+/**
+ * Multi-select, re-orderable select menu.
+ *
+ * @param {object} props                                     - MultiSelect options.
+ * @param {string?} [props.label]                            - Label displayed above the control.
+ * @param {string?} [props.help]                             - Help text displayed below the control.
+ * @param {array<{string, string}>?} props.options           - Options to choose from. Option should be in `{label: '', value: ''}` format.
+ * @param {object} props.value                               - Current value
+ * @param {function} props.onChange                          - Function called when the selection is changed.
+ * @param {boolean} [props.noClear=false]                    - If `true`, the items cannot be cleared/deleted all at once.
+ * @param {boolean} [props.noSearch=false]                   - If `true`, the search functionality is disabled.
+ * @param {boolean} [props.disabled=false]                   - If set `true`, the component is disabled.
+ * @param {boolean} [props.keepMenuOpenAfterSelect=false]    - If set `true`, the dropdown is not closed immediately after selecting an option.
+ * @param {string} [props.noOptionsMessage='No options']     - Text to display when no options are available.
+ * @param {React.Component?} [props.customDropdownIndicator] - If provided, replaces the default dropdown arrow indicator.
+ * @param {React.Component?} [props.customClearIndicator]    - If provided and `noClear` is `false`, replaces the default 'Clear all' button.
+ * @param {React.Component?} [props.customMenuOption]        - If provided, replaces the default item in the dropdown menu (react-select's `components.Option`).
+ * @param {React.Component?} [props.customValueDisplay]      - If provided, replaces the default current value display of each selected item (react-select's `components.MultiValue`).
+ * @param {React.Component?} [props.customValueContainer]    - If provided, replaces the default items container component (react-select's `components.MultiValueContainer`).
+ * @param {React.Component?} [props.customValueRemove]       - If provided, replaces the default item remove button (react-select's `components.MultiValueRemove`.
+ * @param {boolean} [props.noBottomSpacing=false]            - If `true`, the default bottom spacing is removed.
+ * @param {string?} [props.additionalClasses='']             - If provided, the classes are added to the component container.
+ * @param {string?} [props.additionalSelectClasses='']       - If provided, the classes are added to the Select element itself.
+ * @param {object?} [props.additionalProps={}]               - If provided, the provided props will be passed to the Select control.
+ */
 export const MultiSelect = (props) => {
 	const {
 		label,
@@ -23,125 +48,65 @@ export const MultiSelect = (props) => {
 
 		disabled = false,
 
-		closeAfterSelect = false,
+		keepMenuOpenAfterSelect = false,
 
 		noOptionsMessage = __('No options', 'eightshift-frontend-libs'),
 
 		customClearIndicator,
 		customDropdownArrow,
-		customIndicatorSeparator,
 
 		customMenuOption,
 		customValueDisplay,
 		customValueRemove,
 		customValueContainer,
 
-		additionalClass,
+		noBottomSpacing = false,
+
+		additionalClasses,
+		additionalSelectClasses,
 		additionalProps,
 	} = props;
 
-	const handleDragEnd = (event) => {
-		const { active, over } = event;
-
-		if (active.id !== over.id && value) {
-			const mappedItems = value.map(({ id }) => id);
-			const oldIndex = mappedItems?.indexOf(active.id) ?? -1;
-			const newIndex = mappedItems?.indexOf(over.id) ?? -1;
-
-			onChange(arrayMove([...value], oldIndex, newIndex));
-		}
-	};
-
-	const MultiValue = (props) => {
-		const onMouseDown = (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-		};
-
-		const ComponentToRender = customValueDisplay ?? components.MultiValue;
-
-		const innerProps = { ...props.innerProps, onMouseDown };
-
-		const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.data.id });
-
-		const style = {
-			transform: CSS.Transform.toString(transform),
-			transition,
-		};
-
-		return (
-			<div style={style} ref={setNodeRef} {...attributes} {...listeners}>
-				<ComponentToRender {...props} innerProps={innerProps} />
-			</div>
-		);
-	};
-
-	const MultiValueContainer = (props) => {
-		const { setNodeRef } = useDroppable({ id: 'droppable' });
-
-		const ComponentToRender = customValueContainer ?? components.MultiValueContainer;
-
-		return (
-			<div ref={setNodeRef}>
-				<ComponentToRender {...props} />
-			</div>
-		);
-	};
-
-	const MultiValueRemove = (props) => {
-		const ComponentToRender = customValueRemove ?? components.MultiValueRemove;
-
-		return (
-			<ComponentToRender
-				{...props}
-				innerProps={{
-					onPointerDown: (e) => e.stopPropagation(),
-					...props.innerProps,
-				}} />
-		);
-	};
-
 	return (
-		<>
+		<div className={`${noBottomSpacing ? '' : 'es-mb-5'} ${additionalClasses ?? ''}`}>
 			{label &&
-				<span>{label}</span>
+				<span className='es-mb-3'>{label}</span>
 			}
 
-			<DndContext modifiers={[restrictToParentElement]} onDragEnd={handleDragEnd}>
+			<DndContext modifiers={[restrictToParentElement]} onDragEnd={getDragEndHandler(onChange, value)}>
 				<SortableContext items={value.map(({ id }) => id)}>
 					<Select
 						isMulti
 						options={options.map((item) => ({ id: item.value, ...item }))}
 						value={value}
 						onChange={onChange}
-						closeMenuOnSelect={closeAfterSelect}
+						closeMenuOnSelect={!keepMenuOpenAfterSelect}
 						isClearable={!noClear}
 						isSearchable={!noSearch}
 						isDisabled={disabled}
-						className={`${additionalClass ?? ''}`}
+						className={additionalSelectClasses}
 						noOptionsMessage={() => (<span>{noOptionsMessage}</span>)}
 						theme={defaultEightshiftColorScheme}
 						styles={defaultEightshiftStyles}
 						components={{
-							MultiValue,
-							MultiValueContainer,
-							MultiValueRemove,
+							MultiValue: getMultiValue(customValueDisplay ?? components.MultiValue),
+							MultiValueContainer: getMultiValueContainer(customValueContainer ?? components.MultiValueContainer),
+							MultiValueRemove: getMultiValueRemove(customValueRemove ?? components.MultiValueRemove),
 
 							Option: customMenuOption ?? components.Option,
 
-							IndicatorSeparator: customIndicatorSeparator ?? null,
+							IndicatorSeparator: null,
 							DropdownIndicator: customDropdownArrow ?? CustomSelectDefaultDropdownIndicator,
 							ClearIndicator: customClearIndicator ?? CustomSelectDefaultClearIndicator,
 						}}
-
 						{...additionalProps}
 					/>
 				</SortableContext>
 			</DndContext>
 
 			{help &&
-				<span>{help}</span>
+				<span className='es-mt-3'>{help}</span>
 			}
-		</>
+		</div>
 	);
 };
---
+++ scripts/components/custom-select/reorderable-select-components.js
@@ -0,0 +1,67 @@
+import { arrayMove } from '@dnd-kit/sortable';
+import { useSortable } from '@dnd-kit/sortable';
+import { useDroppable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+
+export const getDragEndHandler = (onChange, items) => {
+	return (event) => {
+		const { active, over } = event;
+
+		if (active.id !== over.id && items) {
+			const mappedItems = items.map(({ id }) => id);
+			const oldIndex = mappedItems?.indexOf(active.id) ?? -1;
+			const newIndex = mappedItems?.indexOf(over.id) ?? -1;
+
+			onChange(arrayMove([...items], oldIndex, newIndex));
+		}
+
+	};
+};
+
+export const getMultiValue = (ComponentToRender) => {
+	return (props) => {
+		const onMouseDown = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+		};
+
+		const innerProps = { ...props.innerProps, onMouseDown };
+
+		const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.data.id });
+
+		const style = {
+			transform: CSS.Transform.toString(transform),
+			transition,
+		};
+
+		return (
+			<div style={style} ref={setNodeRef} {...attributes} {...listeners}>
+				<ComponentToRender {...props} innerProps={innerProps} />
+			</div>
+		);
+	};
+};
+
+export const getMultiValueContainer = (ComponentToRender) => {
+	return (props) => {
+		const { setNodeRef } = useDroppable({ id: 'droppable' });
+
+		return (
+			<div ref={setNodeRef}>
+				<ComponentToRender {...props} />
+			</div>
+		);
+	};
+};
+
+export const getMultiValueRemove = (ComponentToRender) => {
+	return (props) => (
+		<ComponentToRender
+			{...props}
+			innerProps={{
+				onPointerDown: (e) => e.stopPropagation(),
+				...props.innerProps,
+			}}
+		/>
+	);
+};
