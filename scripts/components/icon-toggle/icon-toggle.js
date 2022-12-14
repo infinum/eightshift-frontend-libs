3606644a20f784b8c4668c80c6831778b3709bdf
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-14 01:12:44 +0100

    clean up icontoggle

--- scripts/components/icon-toggle/icon-toggle.js
+++ scripts/components/icon-toggle/icon-toggle.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { ToggleControl, CheckboxControl, Button } from '@wordpress/components';
 import { IconLabel } from '../../../scripts';
-import classnames from 'classnames';
 
 /**
  * Custom toggle control that can render as
  *
  * @param {object} props                                                                 - IconToggle options.
+ * @param {React.Component} props.icon                                                   - Icon to display.
  * @param {string} props.label                                                           - Usually component name.
+ * @param {'toggle'|'checkbox'|'button'|'iconButton'|'tileButton'} [props.type='toggle'] - Kind of toggle to render.
+ * @param {string?} props.help                                                           - Help text to display.
+ * @param {boolean} [props.inlineHelp=false]                                             - If `true` and `help` is provided, the help content is rended below the label, instead of below the component.
  * @param {boolean} props.checked                                                        - Is the component currently in use.
  * @param {function} props.onChange                                                      - `onChange` handler from the `ToggleSwitch`/`CheckboxControl`.
- * @param {React.Component} props.icon                                                   - Icon to display.
- * @param {string?} props.help                                                           - Help text to display.
  * @param {boolean} [props.disabled=false]                                               - If `true`, control is disabled.
- * @param {boolean} [props.isCheckbox=false]                                             - Deprecated, please use `type` instead! - If `true`, the control is rendered as a checkbox.
- * @param {boolean} [props.inlineHelp=false]                                             - If `true` and `help` is provided, the help content is rended below the label, instead of below the component.
+ * @param {boolean} [props.noBottomSpacing=false]                                        - If `true`, the default bottom spacing is removed.
  * @param {string?} [props.additionalClasses]                                            - If provided, classes are passed to the underlying component.
- * @param {'toggle'|'checkbox'|'button'|'iconButton'|'tileButton'} [props.type='toggle'] - Kind of toggle to render.
  */
 export const IconToggle = ({
+	icon,
 	label,
+
+	type = 'toggle',
+
+	help,
+	inlineHelp = false,
+
 	checked,
 	onChange,
-	icon,
-	help,
+
 	disabled = false,
-	isCheckbox = false,
-	inlineHelp = false,
+
+	noBottomSpacing = false,
+
 	additionalClasses,
-	type = 'toggle',
 }) => {
 	if (type === 'tileButton') {
 		return (
@@ -58,16 +63,16 @@ export const IconToggle = ({
 		);
 	}
 
-	const ComponentToRender = (isCheckbox || type === 'checkbox') ? CheckboxControl : ToggleControl;
+	const ComponentToRender = (type === 'checkbox') ? CheckboxControl : ToggleControl;
 
 	return (
 		<ComponentToRender
 			checked={checked}
 			onChange={onChange}
 			disabled={disabled}
-			help={!inlineHelp && help}
+			help={!inlineHelp && <span className='es-text-3 es-color-cool-gray-450 -es-mt-1.5! es-display-block'>{help}</span>}
 			label={icon ? (<IconLabel icon={icon} label={label} subtitle={inlineHelp && help} standalone />) : label}
-			className={classnames(['es-mb-6 es-icon-toggle-checkbox', help ? 'es-icon-toggle-checkbox--with-help' : '', additionalClasses ?? ''])}
+			className={`${noBottomSpacing ? 'es-mb-0!' : 'es-mb-5!'} ${additionalClasses ?? ''}`}
 		/>
 	);
 };
