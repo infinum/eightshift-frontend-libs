9dbbbe95da17720b52a4d67d1859090cd7fe6496
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-14 11:25:36 +0100

    add final implementation of async mutliselect

--- scripts/components/custom-select/async-multi-select.js
+++ scripts/components/custom-select/async-multi-select.js
@@ -10,36 +10,40 @@ import { CustomSelectDefaultClearIndicator, CustomSelectDefaultDropdownIndicator
 import { getDragEndHandler, getMultiValue, getMultiValueContainer, getMultiValueRemove } from './reorderable-select-components';
 
 /**
- * Multi-select, re-orderable select menu.
+ * Multi-select, asynchronously-loading, re-orderable select menu.
  *
- * @param {object} props                                     - AsyncMultiSelect options.
- * @param {string?} [props.label]                            - Label displayed above the control.
- * @param {string?} [props.help]                             - Help text displayed below the control.
- * @param {callback<Promise<>>?} props.loadOptions           - An async callback that fetches an array of `{label: '', value: ''}`-formatted items.
- * @param {object} props.value                               - Current value
- * @param {function} props.onChange                          - Function called when the selection is changed.
- * @param {boolean} [props.noClear=false]                    - If `true`, the items cannot be cleared/deleted all at once.
- * @param {boolean} [props.noSearch=false]                   - If `true`, the search functionality is disabled.
- * @param {boolean} [props.disabled=false]                   - If set `true`, the component is disabled.
- * @param {boolean} [props.keepMenuOpenAfterSelect=false]    - If set `true`, the dropdown is not closed immediately after selecting an option.
- * @param {string} [props.noOptionsMessage='No options']     - Text to display when no options are available.
- * @param {string} [props.loadingMessage='Loading']          - Text to display while loading options.
- * @param {React.Component?} [props.customDropdownIndicator] - If provided, replaces the default dropdown arrow indicator.
- * @param {React.Component?} [props.customClearIndicator]    - If provided and `noClear` is `false`, replaces the default 'Clear all' button.
- * @param {React.Component?} [props.customMenuOption]        - If provided, replaces the default item in the dropdown menu (react-select's `components.Option`).
- * @param {React.Component?} [props.customValueDisplay]      - If provided, replaces the default current value display of each selected item (react-select's `components.MultiValue`).
- * @param {React.Component?} [props.customValueContainer]    - If provided, replaces the default items container component (react-select's `components.MultiValueContainer`).
- * @param {React.Component?} [props.customValueRemove]       - If provided, replaces the default item remove button (react-select's `components.MultiValueRemove`.
- * @param {boolean} [props.noBottomSpacing=false]            - If `true`, the default bottom spacing is removed.
- * @param {string?} [props.additionalClasses='']             - If provided, the classes are added to the component container.
- * @param {string?} [props.additionalSelectClasses='']       - If provided, the classes are added to the Select element itself.
- * @param {object?} [props.additionalProps={}]               - If provided, the provided props will be passed to the Select control.
+ * @param {object} props                                               - AsyncMultiSelect options.
+ * @param {string?} [props.label]                                      - Label displayed above the control.
+ * @param {string?} [props.help]                                       - Help text displayed below the control.
+ * @param {boolean|array<{string,string}>} [props.preloadOptions=true] - If `true`, the initial loading is done as soon as the component is loaded. If an array of `{label: '', value: ''}` option is provided, that is loaded immediately, dynamic fetching only happens in search. If `false`, nothing is loaded immediately, until you type to search.
+ * @param {callback<Promise<>>?} props.loadOptions                     - An async callback that fetches an array of `{label: '', value: ''}`-formatted items.
+ * @param {object} props.value                                         - Current value
+ * @param {function} props.onChange                                    - Function called when the selection is changed.
+ * @param {boolean} [props.noClear=false]                              - If `true`, the items cannot be cleared/deleted all at once.
+ * @param {boolean} [props.noSearch=false]                             - If `true`, the search functionality is disabled.
+ * @param {boolean} [props.noOptionCaching=false]                      - If `true`, react-select option caching functionality is disabled.
+ * @param {boolean} [props.disabled=false]                             - If set `true`, the component is disabled.
+ * @param {boolean} [props.keepMenuOpenAfterSelect=false]              - If set `true`, the dropdown is not closed immediately after selecting an option.
+ * @param {string} [props.noOptionsMessage='No options']               - Text to display when no options are available.
+ * @param {string} [props.loadingMessage='Loading']                    - Text to display while loading options.
+ * @param {React.Component?} [props.customDropdownIndicator]           - If provided, replaces the default dropdown arrow indicator.
+ * @param {React.Component?} [props.customClearIndicator]              - If provided and `noClear` is `false`, replaces the default 'Clear all' button.
+ * @param {React.Component?} [props.customMenuOption]                  - If provided, replaces the default item in the dropdown menu (react-select's `components.Option`).
+ * @param {React.Component?} [props.customValueDisplay]                - If provided, replaces the default current value display of each selected item (react-select's `components.MultiValue`).
+ * @param {React.Component?} [props.customValueContainer]              - If provided, replaces the default items container component (react-select's `components.MultiValueContainer`).
+ * @param {React.Component?} [props.customValueRemove]                 - If provided, replaces the default item remove button (react-select's `components.MultiValueRemove`.
+ * @param {boolean} [props.noBottomSpacing=false]                      - If `true`, the default bottom spacing is removed.
+ * @param {function} [props.processLoadedOptions]                      - Allows modifying (filtering, grouping, ...) options output after the items have been dynamically fetched. Please make sure to include `label`, `value` keys and `id` keys, additional fields can be added as required.
+ * @param {string?} [props.additionalClasses='']                       - If provided, the classes are added to the component container.
+ * @param {string?} [props.additionalSelectClasses='']                 - If provided, the classes are added to the Select element itself.
+ * @param {object?} [props.additionalProps={}]                         - If provided, the provided props will be passed to the Select control.
  */
 export const AsyncMultiSelect = (props) => {
 	const {
 		label,
 		help,
 
+		preloadOptions = true,
 		loadOptions,
 		value,
 
@@ -47,6 +51,7 @@ export const AsyncMultiSelect = (props) => {
 
 		noClear = false,
 		noSearch = false,
+		noOptionCaching = false,
 
 		disabled = false,
 
@@ -64,10 +69,17 @@ export const AsyncMultiSelect = (props) => {
 
 		noBottomSpacing = false,
 
+		processLoadedOptions = (options) => options,
+
 		additionalClasses,
 		additionalSelectClasses,
 		additionalProps,
 	} = props;
+
+	const customLoadOptions = async (searchText) => {
+		const results = await loadOptions(searchText);
+		return processLoadedOptions(results?.map((item) => ({ id: item.value, ...item })) ?? []);
+	};
 
 	return (
 		<div className={`${noBottomSpacing ? '' : 'es-mb-5'} ${additionalClasses ?? ''}`}>
@@ -79,9 +91,9 @@ export const AsyncMultiSelect = (props) => {
 				<SortableContext items={value.map(({ id }) => id)}>
 					<AsyncSelect
 						isMulti
-
-						// options={options.map((item) => ({ id: item.value, ...item }))}
-						loadOptions={loadOptions}
+						loadOptions={customLoadOptions}
+						cacheOptions={!noOptionCaching}
+						defaultOptions={preloadOptions}
 						value={value}
 						onChange={onChange}
 						closeMenuOnSelect={!keepMenuOpenAfterSelect}
