3093617f2f207e2ab9851de0d07a4f3522d032d9
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-15 14:51:24 +0100

    simplify fancydivider

--- scripts/components/fancy-divider/fancy-divider.js
+++ scripts/components/fancy-divider/fancy-divider.js
@@ -1,14 +1,16 @@
-import { IconLabel } from '../icon-label/icon-label';
 import React from 'react';
+import { IconLabel, classnames } from '@eightshift/frontend-libs/scripts';
 
 /**
  * A simple <hr /> replacement that draws text and an optional icon.
  *
- * @param {object} props                      - FancyDivider options.
- * @param {React.Component?} [props.icon]     - If provided, icon shown on the divider.
- * @param {string} props.label                - Label shown on the divider.
- * @param {React.Component?} [props.subtitle] - If provided, subtitle shown on the divider.
- * @param {string?} [props.additionalClasses] - Allows passing through extra classes.
+ * @param {object} props                          - FancyDivider options.
+ * @param {React.Component?} [props.icon]         - If provided, icon shown on the divider.
+ * @param {string} props.label                    - Label shown on the divider.
+ * @param {React.Component?} [props.subtitle]     - If provided, subtitle shown on the divider.
+ * @param {boolean} [props.noTopSpacing=false]    - If `true`, the default top spacing is removed.
+ * @param {boolean} [props.noBottomSpacing=false] - If `true`, the default bottom spacing is removed.
+ * @param {string?} [props.additionalClasses]     - Allows passing through extra classes.
  */
 export const FancyDivider = (props) => {
 	const {
@@ -16,25 +18,14 @@ export const FancyDivider = (props) => {
 		label,
 		subtitle,
 
+		noTopSpacing = false,
+		noBottomSpacing = false,
+
 		additionalClasses,
 	} = props;
 
-	if (!icon) {
-		return (
-			<div className={`es-h-spaced es-mt-3 es-mb-2.5 es-nested-p-0.75 es-nested-bg-cool-gray-450 es-nested-rounded-1 es-nested-color-pure-white es-color-cool-gray-600 ${additionalClasses ?? ''}`}>
-				{!subtitle && label}
-
-				{subtitle &&
-					<IconLabel label={label} subtitle={subtitle} />
-				}
-
-				<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
-			</div>
-		);
-	}
-
 	return (
-		<div className={`es-h-spaced es-mt-3 es-mb-2.5 es-nested-p-0.75 es-color-cool-gray-600 ${additionalClasses ?? ''}`}>
+		<div className={classnames('es-h-spaced es-nested-p-0.75 es-nested-bg-cool-gray-450 es-nested-rounded-1 es-nested-color-pure-white es-color-cool-gray-600', !noTopSpacing && 'es-mt-3', !noBottomSpacing && 'es-mb-2.5', additionalClasses)}>
 			<IconLabel icon={icon} label={label} subtitle={subtitle} standalone additionalClasses='es-nested-bg-cool-gray-450 es-nested-rounded-1 es-nested-color-pure-white!' />
 
 			<div className='es-w-full es-flex-1 es-h-px es-bg-cool-gray-50'></div>
