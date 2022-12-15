c9865296406f1398e2f2c9df39479838ff6d9a55
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-12-15 16:09:41 +0100

    convert matrixaligncontrol to use new popover component

--- scripts/components/matrix-align-control/matrix-align-control.js
+++ scripts/components/matrix-align-control/matrix-align-control.js
@@ -1,7 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
-import { Button, Popover } from '@wordpress/components';
-import { icons, OptionSelector } from '../../../scripts';
+import { Button } from '@wordpress/components';
+import { icons, OptionSelector, PopoverWithTrigger } from '../../../scripts';
 import { camelize } from '../../../scripts/helpers';
 import { ucfirst } from '../../../scripts/editor';
 
@@ -28,122 +28,104 @@ export const MatrixAlignControl = (props) => {
 		additionalTriggerClasses,
 	} = props;
 
-	let sizeOptions = [];
-
-	if (size === '3x3') {
-		sizeOptions = [
-			{
-				value: 'top left',
-				label: __('Top-left', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'top center',
-				label: __('Top-center', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'top right',
-				label: __('Top-right', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'center left',
-				label: __('Center-left', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'center center',
-				label: __('Center', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'center right',
-				label: __('Center-right', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'bottom left',
-				label: __('Bottom-left', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'bottom center',
-				label: __('Bottom-center', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'bottom right',
-				label: __('Bottom-right', 'eightshift-frontend-libs'),
-			},
-		];
-	} else {
-		sizeOptions = [
-			{
-				value: 'top left',
-				label: __('Top-left', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'top right',
-				label: __('Top-right', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'bottom left',
-				label: __('Bottom-left', 'eightshift-frontend-libs'),
-			},
-			{
-				value: 'bottom right',
-				label: __('Bottom-right', 'eightshift-frontend-libs'),
-			},
-		];
-	}
+	const allSizeOptions = [
+		{
+			value: 'top left',
+			label: __('Top-left', 'eightshift-frontend-libs'),
+			availableOn: ['3x3', '2x2']
+		},
+		{
+			value: 'top center',
+			label: __('Top-center', 'eightshift-frontend-libs'),
+			availableOn: ['3x3']
+		},
+		{
+			value: 'top right',
+			label: __('Top-right', 'eightshift-frontend-libs'),
+			availableOn: ['3x3', '2x2']
+		},
+		{
+			value: 'center left',
+			label: __('Center-left', 'eightshift-frontend-libs'),
+			availableOn: ['3x3']
+		},
+		{
+			value: 'center center',
+			label: __('Center', 'eightshift-frontend-libs'),
+			availableOn: ['3x3']
+		},
+		{
+			value: 'center right',
+			label: __('Center-right', 'eightshift-frontend-libs'),
+			availableOn: ['3x3']
+		},
+		{
+			value: 'bottom left',
+			label: __('Bottom-left', 'eightshift-frontend-libs'),
+			availableOn: ['3x3', '2x2']
+		},
+		{
+			value: 'bottom center',
+			label: __('Bottom-center', 'eightshift-frontend-libs'),
+			availableOn: ['3x3']
+		},
+		{
+			value: 'bottom right',
+			label: __('Bottom-right', 'eightshift-frontend-libs'),
+			availableOn: ['3x3', '2x2']
+		},
+	];
 
 	// Set icons for (in)active options.
-	sizeOptions = sizeOptions.map((item) => ({
+	const sizeOptions = allSizeOptions.filter(({availableOn}) => availableOn.includes(size)).map((item) => ({
 		...item,
 		icon: item.value === value ? icons.matrixAlignControlDotActive : icons.matrixAlignControlDotInactive,
 	}));
 
-	const [isOpen, setIsOpen] = useState(false);
-	const ref = useRef();
+	const [currentValue, setCurrentValue] = useState(value);
 
 	return (
-		<>
-			{isOpen &&
-				<Popover
-					onClose={() => setIsOpen(false)}
-					anchor={ref?.current}
-					noArrow={false}
-					position='top center'
-				>
-					<div className='es-popover-content es-min-w-auto! es-min-h-auto! es-p-1.5!'>
-						<OptionSelector
-							options={sizeOptions}
-							onChange={(value) => onChange(value)}
-							value={value ?? (size === '3x3' ? 'center center' : 'top left')}
-							border='none'
-							alignment='center'
-							additionalClass={size === '3x3' ? 'es-w-27!' : 'es-w-18!'}
-							additionalButtonClass='es-rounded-1!'
-							noBottomSpacing
-							iconOnly
-						/>
-					</div>
-				</Popover>
-			}
+		<PopoverWithTrigger
+			additionalCloseActions={() => onChange(currentValue)}
+			contentClass='es-min-w-auto! es-min-h-auto! es-p-1.5!'
+			trigger={
+				({ ref, setIsOpen, isOpen }) => {
+					if (type === 'wp') {
+						return (
+							<Button
+								className={additionalTriggerClasses}
+								onClick={() => setIsOpen(!isOpen)}
+								ref={ref}
+								label={label}
+								icon={icons[`position${size}${ucfirst(camelize(value))}`]}
+							/>
+						);
+					}
 
-			{type === 'wp' &&
-				<Button
-					className={additionalTriggerClasses}
-					onClick={() => setIsOpen(!isOpen)}
-					ref={ref}
-					label={label}
-					icon={icons[`position${size}${ucfirst(camelize(value))}`]}
-				/>
+					return (
+						<Button
+							icon={icons[`position${size}${ucfirst(camelize(value))}`]}
+							onClick={() => setIsOpen(!isOpen)}
+							ref={ref}
+							className={`es-button-icon-24 es-slight-button-border-cool-gray-300 es-hover-slight-button-border-cool-gray-500 es-flex-grow-0 es-flex-shrink-0 es-rounded-1! es-has-v2-gutenberg-button-active-state es-flex-col es-gap-1.25! es-w-17! es-h-17! es-button-no-icon-spacing es-content-center! es-text-3! es-line-h-1 es-p-0! es-nested-flex-shrink-0 ${additionalTriggerClasses}`}
+						>
+							{label}
+						</Button>
+					);
+				}
 			}
-
-			{type === 'tileButton' &&
-				<Button
-					icon={icons[`position${size}${ucfirst(camelize(value))}`]}
-					onClick={() => setIsOpen(!isOpen)}
-					ref={ref}
-					className={`es-button-icon-24 es-slight-button-border-cool-gray-300 es-hover-slight-button-border-cool-gray-500 es-flex-grow-0 es-flex-shrink-0 es-rounded-1! es-has-v2-gutenberg-button-active-state es-flex-col es-gap-1.25! es-w-17! es-h-17! es-button-no-icon-spacing es-content-center! es-text-3! es-line-h-1 es-p-0! es-nested-flex-shrink-0 ${additionalTriggerClasses}`}
-				>
-					{label}
-				</Button>
-			}
-		</>
+		>
+			<OptionSelector
+				options={sizeOptions}
+				onChange={(value) => setCurrentValue(value)}
+				value={currentValue ?? (size === '3x3' ? 'center center' : 'top left')}
+				border='none'
+				alignment='center'
+				additionalClass={size === '3x3' ? 'es-w-27!' : 'es-w-18!'}
+				additionalButtonClass='es-rounded-1!'
+				noBottomSpacing
+				iconOnly
+			/>
+		</PopoverWithTrigger>
 	);
 };
