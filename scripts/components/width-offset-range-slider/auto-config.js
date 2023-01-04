b6f431c10a217c5f285112d8e35d5957755a2c9a
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-04 16:05:55 +0100

    add auto-config doc comment and make showFullwidth false by default

--- scripts/components/width-offset-range-slider/auto-config.js
+++ scripts/components/width-offset-range-slider/auto-config.js
@@ -2,22 +2,32 @@ import { checkAttrResponsive, getDefaultBreakpointNames, ucfirst } from '@eights
 
 /**
  *
- * @param {Object} props Generation settings.
+ * @param {Object} options                            - Generation configuration.
+ * @param {string} [options.isFullWidthAttributeName] - Full-width property attribute name.
+ * @param {string} options.offsetAttributeName        - Offset property attribute name.
+ * @param {string} options.widthAttributeName         - Width property attribute name.
+ * @param {Boolean} [options.showFullWidth=false]     - If `true`, the full-width toggle is shown.
+ * @param {Number} [options.min=1]                    - Minimum value of the slider.
+ * @param {Number} [options.numOfColumns=12]          - Number of columns available to choose from.
+ * @param {Object} options.manifest                   - Component/block manifest.
+ * @param {Object} options.attributes                 - Component/block attributes object.
+ * @param {function} options.setAttributes            - Component/block setAttributes function.
+ * @param {string[]?} [options.breakpointNames]       - Breakpoint names to show.
  * @returns Configuration object.
  */
-export const generateWidthOffsetRangeSliderConfig = (props) => {
+export const generateWidthOffsetRangeSliderConfig = (options) => {
 	const {
 		isFullWidthAttributeName,
 		offsetAttributeName,
 		widthAttributeName,
-		showFullWidth,
+		showFullWidth = false,
 		min = 1,
 		numOfColumns = 12,
 		manifest,
 		attributes,
 		setAttributes,
 		breakpointNames = getDefaultBreakpointNames(),
-	} = props;
+	} = options;
 
 	const widths = checkAttrResponsive(widthAttributeName, attributes, manifest, true);
 	const offsets = checkAttrResponsive(offsetAttributeName, attributes, manifest, true);
