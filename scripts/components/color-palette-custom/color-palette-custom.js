7cbba6dbfce4460a8fd2f53b505d10ebc67fb709
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-11 12:56:04 +0100

    rework colorpalette

--- scripts/components/color-palette-custom/color-palette-custom.js
+++ scripts/components/color-palette-custom/color-palette-custom.js
@@ -1,42 +1,31 @@
 import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
-import { BaseControl, Button, TextControl } from '@wordpress/components';
+import { Button, TextControl, Tooltip } from '@wordpress/components';
 import { icons } from '@eightshift/frontend-libs/scripts';
 import classnames from 'classnames';
 import { select } from '@wordpress/data';
 import { STORE_NAME } from './../../editor/store';
-
-/**
- * Determines the color picker layout.
- *
- * - `DEFAULT` - colors are shown as circles without labels, each one has a tooltip.
- * - `LIST` - colors as shown as a single-column list with color names visible.
- * - `LIST_TWO_COL` - colors as shown as a two-column list with color names visible.
- */
-export const ColorPaletteCustomLayout = {
-	DEFAULT: 'default',
-	LIST: 'list',
-	LIST_TWO_COL: 'list-two-col',
-};
+import { Control } from '../base-control/base-control';
+import { ColorSwatch } from '../color-swatch/color-swatch';
 
 /**
  * Modified version of WordPress's `ColorPalette` which saves values as color names/slugs instead of hex codes.
  *
- * @param {object} props                                                             - ColorPaletteCustom options.
- * @param {array} props.colors                                                       - Colors to display.
- * @param {object} props.value                                                       - Current value.
- * @param {function} props.onChange                                                  - Function called when the value is modified.
- * @param {boolean} [props.clearable=false]                                          - If `true`, the color palette will have a *Clear* button.
- * @param {string?} [props.label]                                                    - Label displayed above the picker.
- * @param {string?} [props.help]                                                     - Help text displayed below the picker.
- * @param {string?} [props.inline=false]                                             - If `true` and a `label` or `help` is provided, removes the default bottom spacing from WP `BaseControl`.
- * @param {string?} [props.layout='']                                                - If `true` and a `label` or `help` is provided, removes the default bottom spacing from WP `BaseControl`.
- * @param {ColorPaletteCustomLayout} [props.layout=ColorPaletteCustomLayout.DEFAULT] - Determines the layout of the control.
- * @param {boolean} [props.searchable=false]                                         - If `true`, the list of color can be searched through.
- * @param {boolean} [props.disabled]                                                 - If `true`, the component can't be interacted with.
- * @param {boolean} [props.groupShades=true]                                         - If `true`, color swatches will be grouped if there are 2 or more colors with the same beginning of the name, but different ending (-50, -100, ..., -900 or -10, -20, ..., -90).
+ * @typedef {'tiles'|'list'|'listTwoCol'} ColorPaletteLayout
+ *
+ * @param {object} props                              - ColorPalette options.
+ * @param {array} props.colors                        - Colors to display.
+ * @param {object} props.value                        - Current value.
+ * @param {function} props.onChange                   - Function called when the value is modified.
+ * @param {boolean} [props.clearable=false]           - If `true`, the color palette will have a *Clear* button.
+ * @param {string?} [props.label]                     - Label displayed above the picker.
+ * @param {string?} [props.help]                      - Help text displayed below the picker.
+ * @param {ColorPaletteLayout} [props.layout='tiles'] - Determines the layout of the control.
+ * @param {boolean} [props.searchable=false]          - If `true`, the list of color can be searched through.
+ * @param {boolean} [props.disabled]                  - If `true`, the component can't be interacted with.
+ * @param {boolean} [props.noShadeGrouping=false]     - If `false`, color swatches will be grouped if there are 2 or more colors with the same beginning of the name, but different ending (-50, -100, ..., -900 or -10, -20, ..., -90).
  */
-export const ColorPaletteCustom = (props) => {
+export const ColorPalette = (props) => {
 	const {
 		colors = select(STORE_NAME).getSettings().globalVariables.colors,
 		value,
@@ -44,18 +33,21 @@ export const ColorPaletteCustom = (props) => {
 		clearable = false,
 		label,
 		help,
-		inline = false,
-		layout = ColorPaletteCustomLayout.DEFAULT,
+		icon,
+		noBottomSpacing,
+		layout = 'tiles',
 		searchable = false,
 		disabled,
-		groupShades = true,
+		noShadeGrouping = false,
+		subtitle,
+		actions,
 	} = props;
 
 	const colorSuffixRegex = /(?!^.+)(-?(?:50|100|200|300|400|500|600|700|800|900|10|20|30|40|50|60|70|80|90){1})$/gi;
 
-	let groupedColors = {generic: colors};
+	let groupedColors = { generic: colors };
 
-	if (groupShades) {
+	if (!noShadeGrouping) {
 		groupedColors = colors.reduce((output, current) => {
 			// Bailout if a color is invalid.
 			if (current === undefined || current?.name === undefined || current?.slug === undefined) {
@@ -99,12 +91,44 @@ export const ColorPaletteCustom = (props) => {
 	const [filteredColors, setFilteredColors] = useState(groupedColors);
 	const [searchTerm, setSearchTerm] = useState('');
 
-	const detailView = layout === ColorPaletteCustomLayout.LIST || layout === ColorPaletteCustomLayout.LIST_TWO_COL;
+	const detailView = layout === 'list' || layout === 'listTwoCol';
 
-	const searchEmptyIcon = React.cloneElement(icons.searchEmpty, {className: 'es-w-8 es-h-8 es-has-wp-admin-theme-color'});
+	const searchEmptyIcon = React.cloneElement(icons.searchEmpty, { className: 'es-w-8 es-h-8 es-has-wp-admin-theme-color' });
 
-	const control = (
-		<>
+	let layoutClasses = 'es-h-spaced-wrap es-gap-0!';
+
+	if (layout === 'list') {
+		layoutClasses = 'es-v-spaced es-gap-0!';
+	}
+
+	if (layout === 'listTwoCol') {
+		layoutClasses = 'es-fifty-fifty-h-wrap es-gap-0!';
+	}
+
+	return (
+		<Control
+			icon={icon}
+			label={label}
+			help={help}
+			noBottomSpacing={noBottomSpacing}
+			subtitle={subtitle}
+			actions={
+				!clearable ? actions : (
+					<div className='es-h-spaced'>
+						{actions}
+
+						<Button
+							label={__('Reset', 'eightshift-frontend-libs')}
+							onClick={() => onChange(undefined)}
+							icon={icons.clear}
+							className='es-has-v2-gutenberg-button-active-state es-button-square-24 es-button-icon-22 es-slight-button-border-cool-gray-100 es-hover-slight-button-border-cool-gray-300 es-rounded-1!'
+							disabled={disabled}
+							showTooltip
+						/>
+					</div>
+				)
+			}
+		>
 			{searchable &&
 				<TextControl
 					disabled={disabled}
@@ -136,7 +160,7 @@ export const ColorPaletteCustom = (props) => {
 			{Object.values(filteredColors).every((colors) => colors.length < 1) && searchTerm?.length > 0 &&
 				<div className='es-v-center'>
 					{searchEmptyIcon}
-					<span>{__('No matches for', 'eightshift-frontend-libs')} <i>{searchTerm}</i></span>
+					<span>{__('No matches for', 'eightshift-frontend-libs')} <b>{searchTerm}</b></span>
 				</div>
 			}
 
@@ -148,85 +172,63 @@ export const ColorPaletteCustom = (props) => {
 				}
 
 				return (
-					<div className='es-h-between-wrap es-mb-2' key={i}>
-						{!isOtherColors && <p className='es-mt-0 es-mb-0'>{groupName}</p>}
-
-						{!isOtherColors &&
-							<div className='es-color-theme-container'>
-								{groupColors.map(({ name, slug, color }, j) => {
-									return (
-										<Button
-											key={j}
+					<Control label={!isOtherColors && groupName} key={i} additionalClasses='es-mb-1!' additionalLabelClasses='es-mb-1.5!' noBottomSpacing>
+						<div className='es-h-spaced-wrap es-gap-0!'>
+							{groupColors.map(({ name, slug, color }, j) => {
+								return (
+									<Tooltip text={name} key={j}>
+										<button
 											label={name}
 											onClick={() => onChange(slug)}
-											className={`es-button-icon-28 es-button-square-30 ${value === slug ? 'is-generic-swatch-active' : ''}`}
-											icon={icons.genericColorSwatch}
+											className={classnames('es-border-w-0 es-focus-slight-button-border-admin-accent es-custom-transition es-cursor-pointer es-p-2px! es-m-0', value === slug ? 'es-rounded-full!' : 'es-rounded-1.5!')}
 											disabled={disabled}
 											style={{
-												'--selected-color': slug === 'transparent' ? 'transparent' : color,
-												'--current-opacity': value === slug ? 1 : 0,
-												'--checkerboard-opacity': slug === 'transparent' ? 1 : 0,
+												background: 'none',
+												outline: 'none',
+												'--es-transition-property': 'border-radius, border, box-shadow',
+												'--es-transition-duration': value === slug ? '0.6s, 0.3s, 0.3s' : '1s, 0.3s, 0.3s',
 											}}
-										/>
-									);
-								})}
-							</div>
-						}
-					</div>
+										>
+											<ColorSwatch color={color} selected={value === slug} />
+										</button>
+									</Tooltip>
+								);
+							})}
+						</div>
+					</Control>
 				);
 			})}
 
 			{filteredColors?.generic?.length > 0 &&
-				<BaseControl label={Object?.keys(filteredColors)?.length > 1 ? __('Other colors', 'eightshift-frontend-libs') : null}>
-					<div className={classnames([
-						layout === ColorPaletteCustomLayout.DEFAULT ? 'es-color-palette-custom--inline' : '',
-						layout === ColorPaletteCustomLayout.LIST ? 'es-color-palette-custom--list' : '',
-						layout === ColorPaletteCustomLayout.LIST_TWO_COL ? 'es-color-palette-custom--list-two-col' : '',
-					])}>
+				<Control label={Object?.keys(filteredColors)?.length > 1 && __('Other colors', 'eightshift-frontend-libs')} additionalLabelClasses='es-mb-1.5!' noBottomSpacing>
+					<div className={layoutClasses}>
 						{filteredColors.generic.map(({ name, slug, color }, i) => {
 							return (
-								<Button
-									key={i}
-									label={name}
-									onClick={() => onChange(slug)}
-									className={`es-button-icon-28 ${layout === ColorPaletteCustomLayout.DEFAULT ? 'es-button-square-30' : ''} ${value === slug ? 'is-generic-swatch-active' : ''}`}
-									icon={icons.genericColorSwatch}
-									disabled={disabled}
-									style={{
-										'--selected-color': slug === 'transparent' ? 'transparent' : color,
-										'--current-opacity': value === slug ? 1 : 0,
-										'--checkerboard-opacity': slug === 'transparent' ? 1 : 0,
-									}}
-								>
-									{detailView && name}
-								</Button>
+								<Tooltip text={name} key={i}>
+									<button
+										label={name}
+										onClick={() => onChange(slug)}
+										className={classnames('es-border-w-0 es-focus-slight-button-border-admin-accent es-custom-transition es-cursor-pointer es-m-0 es-p-2px!', detailView && 'es-w-max es-h-spaced', value === slug && !detailView ? 'es-rounded-full!' : 'es-rounded-1.5!')}
+										disabled={disabled}
+										style={{
+											background: 'none',
+											outline: 'none',
+											'--es-transition-property': 'border-radius, border, box-shadow',
+											'--es-transition-duration': value === slug ? '0.6s, 0.3s, 0.3s' : '1s, 0.3s, 0.3s',
+										}}
+									>
+										<ColorSwatch color={color} selected={value === slug} />
+
+										{detailView && name}
+									</button>
+								</Tooltip>
 							);
 						})}
 					</div>
-				</BaseControl>
+				</Control>
 			}
 
-			{clearable &&
-				<Button
-					label={__('Reset', 'eightshift-frontend-libs')}
-					onClick={() => onChange(undefined)}
-					icon={icons.reset}
-					className='es-mt-4 es-button-icon-24'
-					disabled={disabled}
-				>
-					{__('Reset', 'eightshift-frontend-libs')}
-				</Button>
-			}
-		</>
-	);
 
-	if (!label && !help) {
-		return control;
-	}
-
-	return (
-		<BaseControl label={label} help={help} className={inline ? 'es-no-field-spacing' : ''}>
-			{control}
-		</BaseControl>
+		</Control>
 	);
 };
