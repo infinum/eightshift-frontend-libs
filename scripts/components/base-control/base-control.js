fe31c5e783b858592dd8943da0fbe074b3b8bd01
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-18 15:11:46 +0100

    don't add bottom margin on control label if inlinelabel is set

--- scripts/components/base-control/base-control.js
+++ scripts/components/base-control/base-control.js
@@ -72,7 +72,7 @@ export const Control = (props) => {
 	return (
 		<div className={classnames(additionalClasses, !noBottomSpacing && 'es-mb-5')}>
 			{(label || actions) &&
-				<div className={classnames('es-mb-2', (icon || subtitle || actions) && 'es-min-h-7', actions && 'es-h-between', additionalLabelClasses)}>
+				<div className={classnames(!inlineLabel && 'es-mb-2', (icon || subtitle || actions) && 'es-min-h-7', actions && 'es-h-between', additionalLabelClasses)}>
 					{label &&
 						<IconLabel icon={icon} label={label} subtitle={subtitle} standalone />
 					}
