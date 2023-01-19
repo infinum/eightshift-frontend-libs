e9a96ca21fda2970a56e882d674034eec9c58d11
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-19 12:20:55 +0100

    allow passing icon, subtitle and actions to collapsable

--- scripts/components/collapsable/collapsable.js
+++ scripts/components/collapsable/collapsable.js
@@ -7,36 +7,49 @@ import { icons, AnimatedContentVisibility, Control, classnames } from '@eightshi
  * A collapsable container for options.
  *
  * @param {object} props                          - Collapsable options.
+ * @param {React.Component?} [props.icon]         - Icon to show next to the label
  * @param {string} props.label                    - Trigger label.
+ * @param {React.Component?} [props.subtitle]     - Subtitle below the label.
  * @param {boolean} [props.noBottomSpacing=false] - If `true`, the default bottom spacing is removed.
  * @param {React.Component} props.children        - Child items that are shown when expanded.
  * @param {string?} [props.additionalClasses]     - If passed, the classes are appended to the component classes.
+ * @param {React.Component?} [props.actions]      - Actions to show to the right of the label.
  * @returns
  */
 export const Collapsable = ({
+	icon,
 	label,
+	subtitle,
 
-	noBottomSpacing = false,
+	noBottomSpacing,
 
 	children,
 	additionalClasses,
+
+	actions,
 }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
 		<Control
+			icon={icon}
 			label={label}
+			subtitle={subtitle}
 			noBottomSpacing={noBottomSpacing}
 			additionalClasses={classnames('es-nested-collapsable', isOpen && 'is-open', additionalClasses)}
 			additionalLabelClasses={classnames(noBottomSpacing && !isOpen && 'es-mb-0!')}
 			actions={
-				<Button
-					onClick={() => setIsOpen(!isOpen)}
-					className={`es-transition-colors es-button-square-28 es-button-icon-24 es-rounded-1.5 es-has-animated-y-flip-icon ${isOpen ? 'is-active es-nested-color-pure-white es-bg-admin-accent' : ''}`}
-					icon={isOpen ? icons.caretDownFill : icons.caretDown}
-					label={isOpen ? __('Hide options', 'eightshift-frontend-libs') : __('Show options', 'eightshift-frontend-libs')}
-					showTooltip
-				/>
+				<>
+					{actions}
+
+					<Button
+						onClick={() => setIsOpen(!isOpen)}
+						className={`es-transition-colors es-button-square-28 es-button-icon-24 es-rounded-1.5 es-has-animated-y-flip-icon ${isOpen ? 'is-active es-nested-color-pure-white es-bg-admin-accent' : ''}`}
+						icon={isOpen ? icons.caretDownFill : icons.caretDown}
+						label={isOpen ? __('Hide options', 'eightshift-frontend-libs') : __('Show options', 'eightshift-frontend-libs')}
+						showTooltip
+					/>
+				</>
 			}
 		>
 			<AnimatedContentVisibility showIf={isOpen}>
