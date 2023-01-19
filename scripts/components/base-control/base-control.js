3839b5c334fa1f7ab62818c46e4e388c6b519c1e
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-19 16:20:27 +0100

    add reducedBottomSpacing option to basecontrol

--- scripts/components/base-control/base-control.js
+++ scripts/components/base-control/base-control.js
@@ -7,17 +7,18 @@ import { IconLabel } from '@eightshift/frontend-libs/scripts';
  *
  * A base component to optionally encase a component with a label and help.
  *
- * @param {object} props                           - Control options.
- * @param {React.Component?} [props.icon]          - Icon to show next to the label
- * @param {React.Component?} [props.label]         - Label to show above component.
- * @param {React.Component?} [props.subtitle]      - Subtitle below the label.
- * @param {React.Component?} [props.actions]       - Actions to show to the right of the label.
- * @param {React.Component?} [props.help]          - Help to show below the control.
- * @param {React.Component?} [props.children]      - Content to show.
- * @param {string?} [props.additionalClasses]      - Classes to add to the control base.
- * @param {string?} [props.additionalLabelClasses] - Classes to add to the control label.
- * @param {boolean?} [props.inlineLabel=false]     - If `true`, the label is displayed inline with the control. In that case `actions` are shown below the control.
- * @param {boolean?} [props.noBottomSpacing=false] - If `true`, space below the control is removed.
+ * @param {object} props                                - Control options.
+ * @param {React.Component?} [props.icon]               - Icon to show next to the label
+ * @param {React.Component?} [props.label]              - Label to show above component.
+ * @param {React.Component?} [props.subtitle]           - Subtitle below the label.
+ * @param {React.Component?} [props.actions]            - Actions to show to the right of the label.
+ * @param {React.Component?} [props.help]               - Help to show below the control.
+ * @param {React.Component?} [props.children]           - Content to show.
+ * @param {string?} [props.additionalClasses]           - Classes to add to the control base.
+ * @param {string?} [props.additionalLabelClasses]      - Classes to add to the control label.
+ * @param {boolean?} [props.inlineLabel=false]          - If `true`, the label is displayed inline with the control. In that case `actions` are shown below the control.
+ * @param {boolean?} [props.noBottomSpacing=false]      - If `true`, space below the control is removed.
+ * @param {boolean?} [props.reducedBottomSpacing=false] - If `true`, space below the control is reduced.
  */
 export const Control = (props) => {
 	const {
@@ -37,15 +38,18 @@ export const Control = (props) => {
 		additionalLabelClasses,
 
 		noBottomSpacing = false,
+		reducedBottomSpacing = false,
 	} = props;
 
 	if (!children) {
 		return null;
 	}
 
+	const bottomSpacingClass = reducedBottomSpacing ? 'es-mb-2' : 'es-mb-5';
+
 	if (inlineLabel) {
 		return (
-			<div className={classnames(additionalClasses, !noBottomSpacing && 'es-mb-5')}>
+			<div className={classnames(additionalClasses, !noBottomSpacing && bottomSpacingClass)}>
 				{label &&
 					<div className={classnames('es-h-between', (icon || subtitle) && 'es-min-h-7')}>
 						<IconLabel icon={icon} label={label} subtitle={subtitle} standalone />
@@ -70,7 +74,7 @@ export const Control = (props) => {
 	}
 
 	return (
-		<div className={classnames(additionalClasses, !noBottomSpacing && 'es-mb-5')}>
+		<div className={classnames(additionalClasses, !noBottomSpacing && bottomSpacingClass)}>
 			{(label || actions) &&
 				<div className={classnames(!inlineLabel && 'es-mb-2', (icon || subtitle || actions) && 'es-min-h-7', actions && 'es-h-between', additionalLabelClasses)}>
 					{label &&
