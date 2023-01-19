d496d79e2fe7dfaee80dfbe1679f20e637f87f48
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-19 11:03:53 +0100

    fix popoverwithtrigger prop doc comment

--- scripts/components/popover-with-trigger/popover-with-trigger.js
+++ scripts/components/popover-with-trigger/popover-with-trigger.js
@@ -6,7 +6,7 @@ import { Popover } from '@wordpress/components';
  *
  * @param {object} props                                      - PopoverWithTrigger options.
  * @param {string?} [props.contentClass='es-popover-content'] - Class applied to inner popover content.
- * @param {AppearOrigin} [props.popoverPosition='top center'] - Position where the popover appears.
+ * @param {AppearOrigin} [props.position='top center']        - Position where the popover appears.
  * @param {boolean?} [props.noArrow=false]                    - If `true`, the popover doesn't render an arrow pointing to the trigger element.
  * @param {React.Component} props.trigger                     - Trigger element. *Needs to return a React component!* `{ ref, setIsOpen, isOpen }` is passed as props. Use `setIsOpen(true)` to open the modal. Attach the `ref` to some place inside the trigger to make sure the positioning is correct.
  * @param {React.Component?} props.children                   - Popover contents.
