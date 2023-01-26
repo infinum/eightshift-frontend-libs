70d93352e8ce3fa50ba646d908ed87584255aab1
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-01-26 03:03:39 +0100

    remove border around block inserter

--- scripts/components/block-inserter/block-inserter.js
+++ scripts/components/block-inserter/block-inserter.js
@@ -49,8 +49,7 @@ export const BlockInserter = (props) => {
 						icon={icons.add}
 						className={classnames(
 							'es-slight-button-border-cool-gray-400 es-hover-slight-button-border-admin-accent es-active-slight-button-border-admin-accent es-focus-slight-button-border-admin-accent',
-							'es-border-pure-white es-border-w-2px es-nested-m-0! es-gap-1.25!',
-							'es-bg-pure-white! es-mx-auto es-text-3.25! es-color-cool-gray-650 es-rounded-1.5! es-flex-shrink-0',
+							'es-nested-m-0! es-gap-1.25! es-bg-pure-white! es-mx-auto es-text-3.25! es-color-cool-gray-650 es-rounded-1.5! es-flex-shrink-0',
 							!hasLabel && !small && 'es-w-10!',
 							small ? 'es-h-8! es-w-8! es-min-w-8! es-button-icon-18' : 'es-button-icon-20 es-h-10! es-px-2!',
 							additionalClasses,
