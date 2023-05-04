98c4ac54af31a0755a2519af97173a4386e3a83b
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-04 10:12:19 +0200

    add preset picker

---
+++ scripts/components/preset-picker/preset-picker.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { Collapsable, Control } from '../../components';
+import { icons } from '../../editor';
+
+/**
+ * A picker for presets defined in the manifest, with additional configurable options.
+ *
+ * @param {object} props                                     - PresetPicker options.
+ * @param {Object} options.manifest                          - Component/block manifest.
+ * @param {string?} [props.configPresetsKey='configPresets'] - The key from manifest used to pull the preset dat afrom.
+ * @param {function} options.setAttributes                   - Component/block setAttributes function.
+ * @param {boolean?} [props.offButton=false]                 - If provided, shows the "Off" button full-width, above all presets. Data needs to be provided in the form of an object: `{label, icon?, attributes}`.
+ * @param {boolean?} [props.showAsCollapsable=false]         - If `true`, the component is rendered as a collapsable dropdown.
+ * @param {boolean?} [props.controlOnly=false]               - If `true`, the presets are shown by themselves, without a base control or Collapsable to wrap it.
+ * @param {React.Component?} [props.icon]                    - Icon to show next to the label
+ * @param {React.Component?} [props.label='Presets']         - Label of the component.
+ * @param {React.Component?} [props.help]                    - Help text to explain that presets will override the current settings. Can be disabled by setting it to `false`.
+ * @param {boolean?} [props.defaultButton=false]             - If `true`, the "Default" button is shown. It pulls the defaults from manifest, but can be customized by sending an object (`{label?, icon?, attributes}`) instead of `true`.
+ * @param {boolean?} [props.noBottomSpacing=false]           - If `true`, space below the control is removed.
+ * @param {boolean?} [props.reducedBottomSpacing=false]      - If `true`, space below the control is reduced.
+ */
+export const PresetPicker = (props) => {
+	const {
+		manifest,
+		configPresetsKey = 'configPresets',
+		setAttributes,
+
+		showAsCollapsable = false,
+		controlOnly = false,
+
+		icon = icons.sliders,
+		label = __('Presets', 'eightshift-frontend-libs'),
+		help = __('Click to apply. Will override all current settings.', 'eightshift-frontend-libs'),
+
+		offButton = false,
+		defaultButton = false,
+
+		noBottomSpacing,
+		reducedBottomSpacing,
+	} = props;
+
+	if (manifest?.[configPresetsKey]?.length < 1) {
+		return null;
+	}
+
+	const defaultManifestAttributes = Object.entries(manifest.attributes).reduce((curr, [k, v]) => {
+		if ('default' in v) {
+			return {
+				...curr,
+				[k]: v.default,
+			};
+		}
+
+		return {
+			...curr,
+			[k]: undefined,
+		};
+	}, {});
+
+	const presetsContent = (
+		<>
+			{offButton && offButton?.label && offButton?.attributes &&
+				<Button
+					icon={offButton?.icon ?? icons.none}
+					onClick={() => setAttributes(offButton.attributes)}
+					className='es-h-start es-mx-0! es-mt-0! es-mb-1! es-nested-w-7 es-nested-h-7 es-h-auto es-w-full es-h-10 es-rounded-1.5 es-border es-border-cool-gray-100 es-hover-border-cool-gray-400 es-transition es-nested-m-0!'
+				>
+					{offButton?.label}
+				</Button>
+			}
+
+			<div className='es-fifty-fifty-h es-gap-1!'>
+				{defaultButton &&
+					<Button
+						icon={defaultButton?.icon ?? icons.reset}
+						onClick={() => setAttributes(defaultButton?.attributes ?? defaultManifestAttributes)}
+						className='es-h-start es-m-0! es-nested-w-7 es-nested-h-7 es-h-auto es-w-full es-h-10 es-rounded-1.5 es-border es-border-cool-gray-100 es-hover-border-cool-gray-400 es-transition es-nested-m-0!'
+					>
+						{defaultButton?.label ?? __('Default', 'eightshift-frontend-libs')}
+					</Button>
+				}
+
+				{manifest[configPresetsKey].map(({ name: presetName, icon: presetIcon, attributes: presetAttrs }, i) => (
+					<Button
+						key={i}
+						icon={icons?.[presetIcon] ?? icons.genericShapesAlt}
+						onClick={() => setAttributes(presetAttrs)}
+						className='es-h-start es-m-0! es-nested-w-7 es-nested-h-7 es-h-auto es-w-full es-h-10 es-rounded-1.5 es-border es-border-cool-gray-100 es-hover-border-cool-gray-400 es-transition es-nested-m-0!'
+					>
+						{presetName}
+					</Button>
+				))}
+			</div>
+		</>
+	);
+
+	if (controlOnly) {
+		return presetsContent;
+	}
+
+	if (showAsCollapsable) {
+		return (
+			<Collapsable
+				label={label}
+				icon={icon}
+				reducedBottomSpacing={reducedBottomSpacing}
+				noBottomSpacing={noBottomSpacing}
+			>
+				{presetsContent}
+				{help && <br />}
+				{help}
+			</Collapsable>
+		);
+	}
+
+	return (
+		<Control
+			label={label}
+			icon={icon}
+			help={help}
+
+			reducedBottomSpacing={reducedBottomSpacing}
+			noBottomSpacing={noBottomSpacing}
+		>
+			{presetsContent}
+		</Control>
+	);
+};
