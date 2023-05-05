dbcbc90d311f3bc558600973bd5e5a8434a7bf62
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-05 13:35:20 +0200

    tweak repeater doc block

--- scripts/components/simple-repeater/simple-repeater.js
+++ scripts/components/simple-repeater/simple-repeater.js
@@ -26,21 +26,21 @@ import {
 /**
  * A simple repeater.
  *
- * @param {object} props                             - Repeater options.
- * @param {React.Component?} [props.icon]            - Icon to display above the repeater.
- * @param {React.Component?|string} props.label      - Label to display above the repeater.
- * @param {React.Component?|string} [props.subtitle] - Subtitle to display under the title.
- * @param {array} props.items                        - Array of items to display.
- * @param {string} props.attributeName               - Name of the attribute for items.
- * @param {function} props.setAttributes             - The `setAttributes` callback from component/block attributes.
- * @param {array<RepeaterItem>} props.children - Child items, mapped from `items`. Contains all the option for child items.
- * @param {boolean} [props.noReordering=false]       - If `true`, the items can't be reordered.
- * @param {boolean} [props.noBottomSpacing]          - If `true`, the default bottom spacing is removed.
- * @param {boolean?} [props.reducedBottomSpacing]    - If `true`, space below the control is reduced.
- * @param {function} [props.handleAdd]               - Callback for providing custom item adding logic.
- * @param {function} [props.handleItemReorder]       - Callback for providing custom item reordering logic.
- * @param {string?} [props.additionalClasses]        - Classes to add to the control base.
- * @param {string?} [props.additionalLabelClasses]   - Classes to add to the control label.
+ * @param {object} props                               - Repeater options.
+ * @param {React.Component?} [props.icon]              - Icon to display above the repeater.
+ * @param {(React.Component?|string)} props.label      - Label to display above the repeater.
+ * @param {(React.Component?|string)} [props.subtitle] - Subtitle to display under the title.
+ * @param {array} props.items                          - Array of items to display.
+ * @param {string} props.attributeName                 - Name of the attribute for items.
+ * @param {function} props.setAttributes               - The `setAttributes` callback from component/block attributes.
+ * @param {React.Component[]} props.children           - Child items, mapped from `items`. Contains all the option for child items.
+ * @param {boolean} [props.noReordering=false]         - If `true`, the items can't be reordered.
+ * @param {boolean} [props.noBottomSpacing]            - If `true`, the default bottom spacing is removed.
+ * @param {boolean?} [props.reducedBottomSpacing]      - If `true`, space below the control is reduced.
+ * @param {function} [props.handleAdd]                 - Callback for providing custom item adding logic.
+ * @param {function} [props.handleItemReorder]         - Callback for providing custom item reordering logic.
+ * @param {string?} [props.additionalClasses]          - Classes to add to the control base.
+ * @param {string?} [props.additionalLabelClasses]     - Classes to add to the control label.
  */
 export const Repeater = (props) => {
 	const {
