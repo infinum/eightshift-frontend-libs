3c01d4a07897bc6dcca92c004654ec16ca47574f
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-10 16:16:11 +0200

    optimize sortable item in repeater

--- scripts/components/simple-repeater/sortable-item.js
+++ scripts/components/simple-repeater/sortable-item.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
-import { Button, Animate } from '@wordpress/components';
-import { IconLabel, icons } from '@eightshift/frontend-libs/scripts';
+import { Button } from '@wordpress/components';
+import { IconLabel, icons, classnames, AnimatedContentVisibility } from '@eightshift/frontend-libs/scripts';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import classnames from 'classnames';
 
 export const SortableItem = (props) => {
 	const {
@@ -45,25 +44,28 @@ export const SortableItem = (props) => {
 	};
 
 	const itemLabel = (
-		<IconLabel icon={icon} label={title} subtitle={subtitle} additionalClasses={`es-nested-color-cool-gray-650 ${additionalLabelClass ?? ''}`} standalone />
+		<IconLabel icon={icon} label={title} subtitle={subtitle} additionalClasses={classnames('es-nested-color-cool-gray-650', additionalLabelClass)} standalone />
 	);
 
 	return (
-		<div ref={setNodeRef} style={style} className={classnames([
-			isFirst ? 'es-rounded-tl-1.5 es-rounded-tr-1.5' : '',
+		<div ref={setNodeRef} style={style} className={classnames(
+			isFirst && 'es-rounded-tl-1.5 es-rounded-tr-1.5',
 			isLast ? 'es-rounded-bl-1.5 es-rounded-br-1.5' : '-es-mb-px',
 			showChildren ? 'es-rounded-1.5! es-border-cool-gray-500' : 'es-border-cool-gray-300',
-			showChildren && !isFirst && !isLast ? 'es-my-2.5!' : '',
-			showChildren && isFirst && !isLast ? 'es-mb-2.5!' : '',
-			showChildren && !isFirst && isLast ? 'es-mt-2.5!' : '',
-		])}>
-			<div className={`es-pl-2 es-py-1 es-pr-0 es-display-flex es-items-center es-gap-1 ${showChildren ? 'es-border-b-cool-gray-50' : 'es-border-b-transparent'}`} >
+			showChildren && !isFirst && !isLast && 'es-my-2.5!',
+			showChildren && isFirst && !isLast && 'es-mb-2.5!',
+			showChildren && !isFirst && isLast && 'es-mt-2.5!',
+		)}>
+			<div className={classnames('es-pl-2 es-py-1 es-pr-0 es-display-flex es-items-center es-gap-1', showChildren ? 'es-border-b-cool-gray-50' : 'es-border-b-transparent')} >
 				{preIcon}
 
 				{noReordering && itemLabel}
 
 				{!noReordering &&
-					<button className={`es-w-full es-button-reset es-text-align-left es-h-between es-user-select-none es-color-current! ${showChildren ? 'es-pointer-events-none' : 'es-cursor-pointer'}`} {...additionalTriggerProps}>
+					<button
+						className={classnames('es-w-full es-button-reset es-text-align-left es-h-between es-user-select-none es-color-current!', showChildren ? 'es-pointer-events-none' : 'es-cursor-pointer')}
+						{...additionalTriggerProps}
+					>
 						{itemLabel}
 
 						{!showChildren &&
@@ -76,37 +78,30 @@ export const SortableItem = (props) => {
 
 				<Button
 					icon={showChildren ? icons.caretDownFill : icons.caretDown}
-					className={`es-button-icon-24 es-has-animated-y-flip-icon es-rounded-1 es-flex-shrink-0 ${showChildren ? 'is-active es-nested-color-admin-accent' : 'es-nested-color-cool-gray-700'}`}
+					className={classnames('es-button-icon-24 es-has-animated-y-flip-icon es-rounded-1 es-flex-shrink-0', showChildren ? 'is-active es-nested-color-admin-accent' : 'es-nested-color-cool-gray-700')}
 					onClick={() => setShowChildren(!showChildren)}
 				/>
 			</div>
 
-			{showChildren &&
-				<Animate type='slide-in' options={{ origin: 'bottom' }} >
-					{({ className }) => (
-						<div className={className}>
-							{props?.children?.filter(Boolean)?.length > 0 &&
-								<div className='es-p-3'>
-									{props.children}
-								</div>
-							}
+			<AnimatedContentVisibility showIf={showChildren}>
+				{props?.children?.filter(Boolean)?.length > 0 &&
+					<div className='es-p-3'>
+						{props.children}
+					</div>
+				}
 
-							{!hideRemove &&
-								<div className='es-pl-3 es-pr-1 es-py-1.5 es-h-end es-border-t-cool-gray-50'>
-									<Button
-										icon={icons.trash}
-										onClick={onRemove}
-										label={__('Remove', 'eightshift-frontend-libs')}
-										showTooltip
-										className='es-button-icon-24 es-nested-color-red-500 es-rounded-1'
-									/>
-								</div>
-							}
-						</div>
-					)}
-				</Animate>
-			}
-
+				{!hideRemove &&
+					<div className='es-pl-3 es-pr-1 es-py-1.5 es-h-end es-border-t-cool-gray-50'>
+						<Button
+							icon={icons.trash}
+							onClick={onRemove}
+							label={__('Remove', 'eightshift-frontend-libs')}
+							showTooltip
+							className='es-button-icon-24 es-nested-color-red-500 es-rounded-1'
+						/>
+					</div>
+				}
+			</AnimatedContentVisibility>
 		</div>
 	);
 };
