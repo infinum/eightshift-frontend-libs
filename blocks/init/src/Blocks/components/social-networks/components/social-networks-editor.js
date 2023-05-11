2c6aa0a22be2133108a208f5834e2784b09fe9c4
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-11 11:48:44 +0200

    add social networks component

---
+++ blocks/init/src/Blocks/components/social-networks/components/social-networks-editor.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { selector, checkAttr, classnames, getAttrKey } from '@eightshift/frontend-libs/scripts';
+import manifest from '../manifest.json';
+
+export const SocialNetworksEditor = (attributes) => {
+	const {
+		componentClass,
+	} = manifest;
+
+	const {
+		selectorClass = componentClass,
+		blockClass,
+		additionalClass,
+		setAttributes,
+	} = attributes;
+
+	const socialNetworksUse = checkAttr('socialNetworksUse', attributes, manifest);
+
+	if (!socialNetworksUse) {
+		return null;
+	}
+
+	const socialNetworksNetworks = checkAttr('socialNetworksNetworks', attributes, manifest);
+
+	const socialNetworksClasses = classnames(
+		componentClass,
+		selector(blockClass, blockClass, selectorClass),
+		additionalClass,
+	);
+
+	const linkClass = classnames(
+		selector(componentClass, componentClass, 'link'),
+		'es-line-h-0 es-transition-opacity',
+	);
+
+	return (
+		<div className={socialNetworksClasses}>
+			{socialNetworksNetworks.map((network, i) => {
+				return (
+					<button
+						key={i}
+						className={classnames(linkClass, !network.enabled && 'es-opacity-10')}
+						dangerouslySetInnerHTML={{ __html: manifest.networks[network.id].icon }}
+						onClick={() => {
+							const newValue = [...socialNetworksNetworks];
+							newValue[i].enabled = !newValue[i].enabled;
+
+							setAttributes({ [getAttrKey('socialNetworksNetworks', attributes, manifest)]: newValue });
+						}}
+					>
+					</button>
+				);
+			})}
+		</div>
+	);
+};
