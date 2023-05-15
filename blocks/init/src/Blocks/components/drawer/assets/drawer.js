4928af079959d5b739ca3b2f6df1cc6fb1ef53eb
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-15 10:20:00 +0200

    rework drawer

--- blocks/init/src/Blocks/components/drawer/assets/drawer.js
+++ blocks/init/src/Blocks/components/drawer/assets/drawer.js
@@ -1,5 +1,3 @@
-import { debounce } from '@eightshift/frontend-libs/scripts/helpers/debounce';
-
 export class Drawer {
 	constructor({ drawerElement }) {
 		this.drawerElement = drawerElement;
@@ -11,42 +9,62 @@ export class Drawer {
 		if (triggerSelector) {
 			this.trigger = document.querySelector(`.${triggerSelector}`);
 		}
-	}
-	
-	preventScroll() {
-		document.body.classList.add(this.CLASS_NO_SCROLL);
-	}
-	
-	enableScroll() {
-		document.body.classList.remove(this.CLASS_NO_SCROLL);
-	}
-	
-	openMobileMenu() {
-		this.trigger?.classList.add(this.CLASS_MENU_OPEN);
-		this.drawerElement.style.display = 'block';
 
-		const addClass = debounce(() => document.body.classList.add(this.CLASS_MENU_OPEN), 300);
-		addClass();
-
-		this.preventScroll();
+		this.supportsHasSelector = CSS.supports('selector(:has(*))');
 	}
-	
-	closeMobileMenu() {
-		this.trigger?.classList.remove(this.CLASS_MENU_OPEN);
-		document.body.classList.remove(this.CLASS_MENU_OPEN);
 
-		const setDisplayNone = debounce(() => this.drawerElement.style.display = 'none', 300);
-		setDisplayNone();
-
-		this.enableScroll();
-	}
-	
 	init() {
 		this.trigger.addEventListener('click', () => {
-			if (this.trigger?.classList.contains(this.CLASS_MENU_OPEN)) {
-				this.closeMobileMenu();
+			const expanded = this.drawerElement.getAttribute('aria-expanded');
+
+			this.trigger.disabled = true;
+
+			if (expanded === 'true') {
+				const closeAnimation = this.drawerElement.animate([
+					{ gridTemplateRows: '1fr' },
+					{ gridTemplateRows: '0fr' }
+				], {
+					duration: 300,
+					easing: 'cubic-bezier(0.36, 0, 0.66, -0.56)', // ease-in-back
+					fill: 'both',
+				});
+
+				closeAnimation.onfinish = () => {
+					this.drawerElement.setAttribute('aria-expanded', false);
+					this.trigger.disabled = false;
+
+					if (!this.supportsHasSelector) {
+						this.trigger?.classList.remove(this.CLASS_MENU_OPEN);
+						document.body.classList.remove(this.CLASS_MENU_OPEN);
+
+						// Enable scroll.
+						document.body.classList.remove(this.CLASS_NO_SCROLL);
+					}
+
+				};
 			} else {
-				this.openMobileMenu();
+				this.drawerElement.setAttribute('aria-expanded', true);
+
+				const enterAnimation = this.drawerElement.animate([
+					{ gridTemplateRows: '0fr' },
+					{ gridTemplateRows: '1fr' }
+				], {
+					duration: 500,
+					easing: 'cubic-bezier(0.34, 1.56, 0.64, 1)', // ease-out-back
+					fill: 'both',
+				});
+
+				enterAnimation.onfinish = () => {
+					this.trigger.disabled = false;
+
+					if (!this.supportsHasSelector) {
+						this.trigger?.classList.add(this.CLASS_MENU_OPEN);
+						document.body.classList.add(this.CLASS_MENU_OPEN);
+
+						// Prevent scroll.
+						document.body.classList.add(this.CLASS_NO_SCROLL);
+					}
+				};
 			}
 		});
 	}
