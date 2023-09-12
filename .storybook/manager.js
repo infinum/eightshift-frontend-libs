d80cda165b3f19439ae25c2d8ac5a8f066bb9de3
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-09-12 15:43:00 +0200

    make storybook look better

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,29 @@
 import { addons } from '@storybook/addons';
 import customEightshiftThemes from './theme';
 
+// Set default sidebar width.
+const storybookLayout = JSON.parse(localStorage['storybook-layout'] || '{}');
+const newLayout = { resizerNav: { x: 310, y: 0 } };
+localStorage['storybook-layout'] = JSON.stringify({ ...storybookLayout, ...newLayout });
+
 addons.setConfig({
 	showPanel: false,
 	theme: customEightshiftThemes,
+	isFullscreen: false,
+	showNav: true,
+	enableShortcuts: false,
+	showToolbar: true,
+	selectedPanel: undefined,
+	initialActive: 'sidebar',
+	sidebar: {
+		showRoots: true,
+		collapsedRoots: ['other'],
+	},
+	toolbar: {
+		zoom: { hidden: true },
+		eject: { hidden: true },
+		copy: { hidden: true },
+		fullscreen: { hidden: true },
+		addons: { hidden: true },
+	},
 });
