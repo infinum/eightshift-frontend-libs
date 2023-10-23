dd422e5f6af184bcfed2cf5626b96a1bf6ecf910
Ivan Kancijan <135589039+kancijan@users.noreply.github.com>
2023-10-23 14:59:55 +0200

    Tweak Load more
    Converted to async await

--- blocks/init/src/Blocks/components/load-more/assets/index.js
+++ blocks/init/src/Blocks/components/load-more/assets/index.js
@@ -3,7 +3,7 @@
 import domReady from '@wordpress/dom-ready';
 import manifest from './../manifest.json';
 
-domReady(() => {
+domReady(async () => {
 	const {
 		componentJsClass,
 	} = manifest;
@@ -11,14 +11,12 @@ domReady(() => {
 	const selectors = `.${componentJsClass}`;
 	const elements = document.querySelectorAll(selectors);
 
-	if (elements.length) {
-		import('./load-more').then(({ LoadMore }) => {
-			const loadMore = new LoadMore({
-				triggerElements: elements,
-				restUrl: esBlocksLocalization?.loadMoreRestUrl ?? '',
-			});
+	const { LoadMore } = await import('./load-more');
 
-			loadMore.init();
-		});
-	}
+	const loadMore = new LoadMore({
+		triggerElements: elements,
+		restUrl: esBlocksLocalization?.loadMoreRestUrl ?? '',
+	});
+
+	loadMore.init();
 });
