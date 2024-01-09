7fb26d7bec3d04a2b272203714c9b600bda5f7bc
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-01-09 15:39:28 +0100

    improve camelCase reliablity

--- scripts/helpers/es-dash.js
+++ scripts/helpers/es-dash.js
@@ -12,14 +12,17 @@
  * Usage:
  * ```js
  * camelCase('New super Test-title') // => 'newSuperTestTitle'
+ * camelCase(null) // => null
  * ```
  */
 export const camelCase = (input) => {
-	const string = input.toLowerCase().replace(/(?:(^.)|([-_\s]+.))/g, function (match) {
-		return match.charAt(match.length - 1).toUpperCase();
-	});
+	if (!input) {
+		return null;
+	}
 
-	return string.charAt(0).toLowerCase() + string.substring(1);
+	const string = input?.toLowerCase()?.replace(/(?:(^.)|([-_\s]+.))/g, (match) => match?.charAt(match.length - 1)?.toUpperCase());
+
+	return string?.charAt(0)?.toLowerCase() + string?.substring(1);
 };
 
 // Source: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore
