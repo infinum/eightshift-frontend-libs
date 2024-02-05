983e6faccd4c1f85de22e509bda4513bae1bc1b5
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-02-06 00:22:37 +0100

    update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,41 @@
-module.exports = {
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
 	stories: [
+		'./../blocks/init/**/readme.mdx',
+		'./../scripts/components/**/readme.mdx',
+		'./../scripts/editor/icons/readme.mdx',
 		'./../blocks/init/**/story.js',
 		'./../scripts/components/**/story.js',
 		'./../scripts/editor/icons/story.js',
 	],
 	addons: [
-		'@storybook/addon-essentials',
-		'@storybook/addon-a11y'
+		{
+			name: '@storybook/addon-essentials',
+			options: {
+				actions: false,
+				backgrounds: false,
+				controls: false,
+				docs: false,
+				viewport: false,
+				toolbars: false,
+				measure: false,
+				outline: false,
+				highlight: false
+			},
+		},
+		'@storybook/addon-a11y',
+		'@storybook/addon-docs',
 	],
-	core: {
-		builder: 'webpack5',
+	framework: {
+		name: '@storybook/react-webpack5',
+		options: {
+			builder: {
+				useSWC: true,
+			},
+		},
 	},
-	features: {
-		emotionAlias: false,
-	}
+	docs: {
+		autodocs: false,
+	},
 };
+export default config;
