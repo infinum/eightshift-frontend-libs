8602c0ea18653c5e9f37967fd3bd2e87b0948063
DjidjaleskaSandra <sandra.djidjaleska@infinum.hr>
2024-04-23 13:46:18 +0200

    Check if the path is wrong and return message

--- setup/create-wp-project/src/basics/files.js
+++ setup/create-wp-project/src/basics/files.js
@@ -13,15 +13,15 @@ const installPath = async (requiredFolder = 'themes') => {
   const currentPath = path.join(process.cwd());
   const currentDirName = path.basename(currentPath);
 
-  if (currentDirName === requiredFolder) {
-    return currentPath;
-  }
+	if (currentDirName === requiredFolder) {
+			return { path: currentPath, isCorrectFolder: true };
+	}
 
   const requiredPath = (currentDirName === 'wp-content') ?
     `${currentPath}/${requiredFolder}` :
     `${currentPath}/wp-content/${requiredFolder}`;
 
-  return requiredPath;
+	return { path: requiredPath, isCorrectFolder: false };
 };
 
 /**
