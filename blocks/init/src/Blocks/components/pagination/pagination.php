c7930236b3bb044d24f5f4d03e5d28a23cf71838
DjidjaleskaSandra <sandra.djidjaleska@infinum.hr>
2024-06-19 21:55:20 +0200

    optimize the code

--- blocks/init/src/Blocks/components/pagination/pagination.php
+++ blocks/init/src/Blocks/components/pagination/pagination.php
@@ -6,49 +6,48 @@
  * @package %g_namespace%
  */
 
-use %g_namespace_vendor_prefix%\EightshiftLibs\Helpers\Helpers;
+ use %g_namespace_vendor_prefix%\EightshiftLibs\Helpers\Helpers;
 
 $manifest = Helpers::getManifestByDir(__DIR__);
-
-$paginationUse = Helpers::checkAttr('paginationUse', $attributes, $manifest);
-if (!$paginationUse) {
-	return;
-}
-
-$paginationIconName = Helpers::checkAttr('paginationIconName', $attributes, $manifest);
-$paginationCurrentPage = Helpers::checkAttr('paginationCurrentPage', $attributes, $manifest);
-$paginationMaxPages = Helpers::checkAttr('paginationMaxPages', $attributes, $manifest);
 
 $componentClass = $manifest['componentClass'] ?? '';
 $additionalClass = $attributes['additionalClass'] ?? '';
 $blockClass = $attributes['blockClass'] ?? '';
 $selectorClass = $attributes['selectorClass'] ?? $componentClass;
 
-$navigationClass = Helpers::classnames(
+$paginationClass = Helpers::classnames([
 	$componentClass,
 	Helpers::selector($blockClass, $blockClass, $selectorClass),
-	$additionalClass,
-);
+	$additionalClass
+]);
+
+$paginationPrevClass = Helpers::selector($componentClass, $componentClass, 'prev');
+$paginationNextClass = Helpers::selector($componentClass, $componentClass, 'next');
+$paginationLinkClass = Helpers::selector($componentClass, $componentClass, 'link');
 
 $icon = Helpers::render(
 	'icon',
 	Helpers::props('icon', $attributes, [
 		'blockClass' => $componentClass,
 		'selectorClass' => 'icon',
-		'iconName' => $paginationIconName,
 	])
 );
 ?>
 
-<div class="<?php echo esc_attr($navigationClass); ?>">
+<div class="<?php echo esc_attr($paginationClass); ?>">
 	<?php
-	echo paginate_links(
-			[
-				'before_page_number' => '<span>',
-				'after_page_number' => '</span>',
-				'prev_text' => '<span>' . $icon . __('Previous', '%g_textdomain%') . '</span>',
-				'next_text' => '<span>' . __('Next', '%g_textdomain%') . $icon . '</span>',
-			]
+	$pagination_links = paginate_links(
+		[
+			'before_page_number' => '<span>',
+			'after_page_number' => '</span>',
+			'prev_text' => '<span class="' . $paginationPrevClass . '" aria-label = ' . __('Previous', '%g_textdomain%') . '>' .  $icon . '</span>',
+			'next_text' => '<span class="' . $paginationNextClass . '"aria-label = ' . __('Next', '%g_textdomain%') . '>' .  $icon . '</span>',
+		]
 	);
+
+	// Add custom css classes.
+	$pagination_links = str_replace('<a', '<a class="' . $paginationLinkClass . '"', $pagination_links);
+
+	echo $pagination_links;
 	?>
 </div>
