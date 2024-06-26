2ed2aec447cecaaf602ed866f774687d13a26a54
Ivan Ružević <ivan.ruzevic@infinum.hr>
2024-06-26 09:08:58 +0200

    updating webpack and babel (#833)
    * updating webpack and babel

--- webpack/index.js
+++ webpack/index.js
@@ -24,7 +24,6 @@ module.exports = (mode, optionsData = {}) => {
 		optionsData.config.blocksAssetsPath,
 		optionsData.config.outputPath,
 		optionsData.config.blocksManifestSettingsPath,
-		optionsData.config.useSsl,
 	);
 
 	options.config.mode = mode;
@@ -33,21 +32,18 @@ module.exports = (mode, optionsData = {}) => {
 	// Get all webpack partials.
 	const base = require('./base')(options);
 	const project = require('./project')(options);
-	const development = require('./development')(options);
 	const production = require('./production')(options);
 
 	// Default output that is going to be merged in any env.
 	const outputDefault = merge(project, base);
 
 	// Output development setup by default.
-	let output;
+	const development = {
+		devtool: false,
+		watchOptions: {
+			ignored: '**/node_modules',
+		},
+	};
 
-	// Output production setup if mode is set inside package.json.
-	if (mode === 'production') {
-		output = merge(outputDefault, production);
-	} else {
-		output = merge(outputDefault, development);
-	}
-
-	return output;
+	return merge(outputDefault, mode === 'production' ? production : development);
 };
