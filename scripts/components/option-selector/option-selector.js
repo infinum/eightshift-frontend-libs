ea1990c1575a9c9a5eb2f248c9174d9f07dc20cb
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-19 17:09:33 +0200

    replace more components

--- scripts/components/option-selector/option-selector.js
+++ scripts/components/option-selector/option-selector.js
@@ -1,36 +1,28 @@
 import React from 'react';
-import { Control, IconLabel } from '@eightshift/frontend-libs/scripts';
-import { Button } from '@wordpress/components';
-import { icons } from '@eightshift/ui-components/icons';
-import { clsx } from '@eightshift/ui-components/utilities';
+import { OptionSelect } from '@eightshift/ui-components';
 
 /**
+ * @deprecated Use `OptionSelect` from `@eightshift/ui-components` instead.
+ *
  * Button-based option selector, inspired by WP 5.9.
  *
- * @param {object} props                                                                       - OptionSelector options.
- * @param {React.Component?} [props.icon]                                                      - Icon to show next to the label
- * @param {React.Component?} [props.help]                                                      - Help text displayed below the control.
- * @param {React.Component?} [props.label]                                                     - Label displayed above the control.
- * @param {boolean?} [props.inlineLabel]                                                       - Label displayed inline with the control.
- * @param {React.Component?} [props.actions]                                                   - Actions to show to the right of the label.
- * @param {React.Component?} [props.subtitle]                                                  - Subtitle below the label.
- * @param {any} [props.value]                                                                  - Current value.
- * @param {function} [props.onChange]                                                          - Function that is called on every value change.
- * @param {boolean} [props.disabled=false]                                                     - If `true`, the component will be disabled.
- * @param {array<string|{label, value, icon, tooltip}>} [props.options]                        - Options to show, either values or objects with {label?, value, icon?, subtitle?}
- * @param {array<{label, value}>?} [props.optionLabels]                                        - If passed, these labels/icons will be used instead the ones provided with `options`. Must be passed when `options` contain just values.
- * @param {'none'|'offset'} [props.border='offset']                                            - Sets the appearance of a border around the buttons.
- * @param {boolean} [props.noWrap=false]                                                       - If `false` and there is more options then can fit, the buttons will wrap to the row below.
- * @param {'default'|'stretch'|'left'|'center'|'right'|'vertical'} [props.alignment='default'] - If `true` and there is more options then can fit, the buttons will wrap to the row below.
- * @param {boolean} [props.iconOnly=false]                                                     - If `true`, the buttons will only contain icons. If a label is also passed, it will be used for the button tooltip.
- * @param {boolean} [props.largerIcons=false]                                                  - If `true`, the icons inside of buttons are rendered larger.
- * @param {boolean} [props.compactButtons=false]                                               - If `true`, the buttons are rendered smaller
- * @param {boolean} [props.labelOnlyOnActive=false]                                            - If `true`, the label is shown only on the active item.
- * @param {boolean} [props.noBottomSpacing]                                                    - If `true`, the default bottom spacing is removed.
- * @param {boolean?} [props.reducedBottomSpacing]                                              - If `true`, space below the control is reduced.
- * @param {string?} [props.additionalClass]                                                    - If provided, the classes are appended to the button container.
- * @param {string?} [props.additionalButtonClass]                                              - If provided, the classes are appended to the selection buttons.
- * @param {string?} [props.additionalContainerClass]                                           - If provided, the classes are appended to the container.
+ * @param {object} props - OptionSelector options.
+ * @param {React.Component?} [props.icon] - Icon to show next to the label
+ * @param {React.Component?} [props.help] - Help text displayed below the control.
+ * @param {React.Component?} [props.label] - Label displayed above the control.
+ * @param {boolean?} [props.inlineLabel] - Label displayed inline with the control.
+ * @param {React.Component?} [props.actions] - Actions to show to the right of the label.
+ * @param {React.Component?} [props.subtitle] - Subtitle below the label.
+ * @param {any} [props.value] - Current value.
+ * @param {function} [props.onChange] - Function that is called on every value change.
+ * @param {boolean} [props.disabled=false] - If `true`, the component will be disabled.
+ * @param {array<string|{label, value, icon, tooltip}>} [props.options] - Options to show, either values or objects with {label?, value, icon?, subtitle?}
+ * @param {'default'|'stretch'|'left'|'center'|'right'|'vertical'} [props.alignment='default'] - Option alignment.
+ * @param {boolean} [props.iconOnly=false] - If `true`, the buttons will only contain icons. If a label is also passed, it will be used for the button tooltip.
+ * @param {boolean} [props.compactButtons=false] - If `true`, the buttons are rendered smaller
+ * @param {string?} [props.additionalClass] - If provided, the classes are appended to the button container.
+ * @param {string?} [props.additionalButtonClass] - If provided, the classes are appended to the selection buttons.
+ * @param {string?} [props.additionalContainerClass] - If provided, the classes are appended to the container.
  */
 export const OptionSelector = (props) => {
 	const {
@@ -47,166 +39,33 @@ export const OptionSelector = (props) => {
 		disabled = false,
 
 		options,
-		optionLabels,
 
-		border = 'offset',
-		noWrap = false,
 		alignment = 'default',
 
 		iconOnly = false,
-		largerIcons = false,
 		compactButtons = false,
-		labelOnlyOnActive = false,
 
-		noBottomSpacing,
-		reducedBottomSpacing,
-
-		additionalClass,
 		additionalButtonClass,
 		additionalContainerClass,
 	} = props;
 
-	const buttonClasses = clsx(
-		iconOnly && 'es-button-square-36',
-		largerIcons && 'es-button-icon-24',
-		border === 'offset' && 'es-rounded-0.75! es-p-1.5!',
-		additionalButtonClass
-	);
-
-	const getSpacingConfig = (alignment) => {
-		switch (alignment) {
-			case 'left':
-				return 'es-mr-auto es-content-start es-w-max';
-			case 'center':
-				return 'es-mx-auto es-content-center es-w-max';
-			case 'right':
-				return 'es-ml-auto es-content-center es-w-max';
-			case 'stretch':
-				return 'es-content-between es-w-full';
-			case 'vertical':
-				return 'es-flex-col es-w-full';
-			default:
-				return 'es-w-max';
-		}
-	};
-
-	const control = (
-		<div
-			className={clsx(
-				'es-option-selector es-display-flex es-gap-px es-has-v2-gutenberg-button-active-state-inside es-max-w-full',
-				!label && !help && additionalClass,
-				noWrap ? 'es-flex-nowrap' : 'es-flex-wrap',
-				border === 'offset' && 'es-border-cool-gray-400 es-p-0.5 es-rounded-1.25',
-				getSpacingConfig(alignment),
-			)}
-		>
-			{options.map((item, i) => {
-				const iconData = icons?.[item?.icon] ?? item?.icon ?? optionLabels?.[i]?.icon;
-
-				let icon = iconData;
-
-				if (iconData && typeof iconData === 'string' && (iconData?.startsWith('<svg') ?? false)) {
-					icon = (
-						<i className='es-line-h-0 es-display-contents' dangerouslySetInnerHTML={{ __html: iconData }} />
-					);
-				}
-
-				const label = item?.label ?? optionLabels?.[i]?.label;
-				const current = optionLabels ? item : item?.value;
-				const tooltip = item?.tooltip ?? optionLabels?.[i]?.tooltip ?? item?.label ?? optionLabels?.[i]?.label;
-				const subtitle = item?.subtitle ?? optionLabels?.[i]?.subtitle;
-
-				if (icon && !label && !subtitle) {
-					return (
-						<Button
-							key={i}
-							onClick={() => onChange(current)}
-							isPressed={value === current}
-							icon={icon}
-							disabled={disabled}
-							size={compactButtons ? 'small' : 'default'}
-							className={buttonClasses}
-						/>
-					);
-				} else if (icon && label && (iconOnly || (labelOnlyOnActive && value !== current)) && !subtitle) {
-					return (
-						<Button
-							key={i}
-							onClick={() => onChange(current)}
-							isPressed={value === current}
-							icon={icon}
-							label={tooltip}
-							disabled={disabled}
-							showTooltip
-							isSmall={compactButtons}
-							className={buttonClasses}
-						/>
-					);
-				} else if (icon && label && !subtitle) {
-					return (
-						<Button
-							key={i}
-							onClick={() => onChange(current)}
-							isPressed={value === current}
-							icon={icon}
-							disabled={disabled}
-							size={compactButtons ? 'small' : 'default'}
-							label={tooltip !== label ? tooltip : null}
-							showTooltip={tooltip !== label}
-							className={buttonClasses}
-						>
-							{label}
-						</Button>
-					);
-				} else if (icon && label && subtitle) {
-					return (
-						<Button
-							key={i}
-							onClick={() => onChange(current)}
-							isPressed={value === current}
-							disabled={disabled}
-							size={compactButtons ? 'small' : 'default'}
-							label={tooltip !== label ? tooltip : null}
-							showTooltip={tooltip !== label}
-							className={buttonClasses}
-						>
-							<IconLabel icon={icon} label={label} subtitle={subtitle} standalone />
-						</Button>
-					);
-				}
-
-				return (
-					<Button
-						key={i}
-						onClick={() => onChange(current)}
-						isPressed={value === current}
-						disabled={disabled}
-						size={compactButtons ? 'small' : 'default'}
-						label={tooltip !== label ? tooltip : null}
-						showTooltip={tooltip !== label}
-						className={buttonClasses}
-					>
-						{label}
-					</Button>
-				);
-			})}
-		</div>
-	);
-
 	return (
-		<Control
+		<OptionSelect
 			icon={icon}
-			label={label}
-			noBottomSpacing={noBottomSpacing}
-			reducedBottomSpacing={reducedBottomSpacing}
-			inlineLabel={inlineLabel}
 			help={help}
-			additionalClasses={additionalContainerClass}
+			label={label}
+			inline={inlineLabel}
 			actions={actions}
 			subtitle={subtitle}
-			additionalLabelClasses={clsx(!inlineLabel && 'es-mb-1!')}
-		>
-			{control}
-		</Control>
+			value={value}
+			onChange={onChange}
+			disabled={disabled}
+			options={options}
+			vertical={alignment === 'vertical'}
+			noItemLabel={iconOnly}
+			itemProps={{ size: compactButtons ? 'small' : 'default' }}
+			className={additionalContainerClass}
+			itemClassName={additionalButtonClass}
+		/>
 	);
 };
